02a181f7dda019e5edb9e7ea1e6410cbfc25ae4f
Putu Eka Mulyana <putuekamulyana97@gmail.com>
2021-03-22 16:48:54 +0800

    update root-stack

--- src/navigations/root-stack.js
+++ src/navigations/root-stack.js
@@ -16,7 +16,7 @@ const RootStact = () => {
     StatusBar.setBarStyle('dark-content', true);
   }, []);
   return (
-    <Stack.Navigator initialRouteName="ChooseUsername">
+    <Stack.Navigator initialRouteName="SplashScreen">
       <Stack.Screen
         name="SplashScreen"
         component={SplashScreen}
