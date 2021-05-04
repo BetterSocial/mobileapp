ba0ef71c9717c3bad79bfa8de72a6847df53b5ba
Putu Eka Mulyana <putuekamulyana97@gmail.com>
2021-05-04 09:31:20 +0800

    screen post Detail

--- src/screens/Post/PostDetailPage.js
+++ src/screens/Post/PostDetailPage.js
@@ -1,5 +1,13 @@
-import React, {useState} from 'react';
-import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import React, {useEffect, useRef, useState} from 'react';
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import JWTDecode from 'jwt-decode';
+import {getAccessToken} from '../../data/local/accessToken';
 import Comment from '../../elements/PostDetail/Comment';
 import ContainerComment from '../../elements/PostDetail/ContainerComment';
 import Footer from '../../elements/PostDetail/Footer';
@@ -7,10 +15,32 @@ import Header from '../../elements/PostDetail/Header';
 import Profile from '../../elements/PostDetail/Profile';
 import WriteComment from '../../elements/PostDetail/WriteComment';
 import {fonts} from '../../utils/fonts';
+import {getMyProfile} from '../../service/profile';
+import BlockUser from '../../elements/Blocking/BlockUser';
 
 const PostDetailPage = () => {
   const [more, setMore] = useState(10);
   const [totalLine, setTotalLine] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [dataProfile, setDataProfile] = useState({});
+  const refBlockUser = useRef();
+  useEffect(() => {
+    fetchMyProfile();
+    refBlockUser.current.open();
+  }, []);
+  const fetchMyProfile = async () => {
+    let token = await getAccessToken();
+    if (token) {
+      var decoded = await JWTDecode(token);
+      const result = await getMyProfile(decoded.user_id);
+      if (result.code === 200) {
+        setDataProfile(result.data);
+        console.log('detai ', result.data);
+        setLoading(false);
+      }
+      setLoading(false);
+    }
+  };
   const onTextLayout = (e) => {
     setTotalLine(e.nativeEvent.lines.length);
   };
@@ -19,36 +49,40 @@ const PostDetailPage = () => {
       setMore(more + 10);
     }
   };
+
   return (
     <View style={styles.container}>
-      <Header />
-      <Profile />
-      <View style={styles.containerText}>
-        <Text
-          style={styles.textDesc}
-          numberOfLines={more}
-          onTextLayout={onTextLayout}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae
-          diam et tortor rutrum tincidunt vitae non arcu. Pellentesque mattis
-          tellus quam, sed porttitor nunc aliquam vitae. Donec id dui lacinia,
-          pellentesque ipsum sed, commodo sapien. Praesent tincidunt accumsan
-          nibh, id laoreet sapien porta et. Ut aliquet purus sit amet lectus
-          fermentum, id consectetur lorem porta. Donec vestibulum lobortis
-          ligula, sit amet luctus enim tincidunt non. Nam ultricies lacus ac
-          nibh molestie volutpat. Ut aliquet purus sit amet lectus fermentum, id
-          consectetur lorem porta. Donec vestibulum lobortis ligula, sit amet
-          luctus enim tincidunt non. Nam ultricies lacus ac nibh molestie
-          volutpat.
-        </Text>
-        {more < totalLine && (
-          <TouchableOpacity onPress={() => onMore()}>
-            <Text style={styles.more}>More</Text>
-          </TouchableOpacity>
-        )}
-      </View>
-      <Footer />
-      <ContainerComment />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Header />
+        <Profile />
+        <View style={styles.containerText}>
+          <Text
+            style={styles.textDesc}
+            numberOfLines={more}
+            onTextLayout={onTextLayout}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vitae
+            diam et tortor rutrum tincidunt vitae non arcu. Pellentesque mattis
+            tellus quam, sed porttitor nunc aliquam vitae. Donec id dui lacinia,
+            pellentesque ipsum sed, commodo sapien. Praesent tincidunt accumsan
+            nibh, id laoreet sapien porta et. Ut aliquet purus sit amet lectus
+            fermentum, id consectetur lorem porta. Donec vestibulum lobortis
+            ligula, sit amet luctus enim tincidunt non. Nam ultricies lacus ac
+            nibh molestie volutpat. Ut aliquet purus sit amet lectus fermentum,
+            id consectetur lorem porta. Donec vestibulum lobortis ligula, sit
+            amet luctus enim tincidunt non. Nam ultricies lacus ac nibh molestie
+            volutpat.
+          </Text>
+          {more < totalLine && (
+            <TouchableOpacity onPress={() => onMore()}>
+              <Text style={styles.more}>More</Text>
+            </TouchableOpacity>
+          )}
+        </View>
+        <Footer />
+        <ContainerComment />
+      </ScrollView>
       <WriteComment />
+      <BlockUser refBlockUser={refBlockUser} />
     </View>
   );
 };
