8ed275fe11b5fd591439ac365f15ea4e91702aaf
usupsuparma <usup.suparma@bijb.co.id>
2021-05-17 10:26:07 +0700

    fixing merge

--- src/screens/Post/PostDetailPage.js
+++ src/screens/Post/PostDetailPage.js
@@ -27,12 +27,10 @@ import Header from '../feedScreen/Header';
 import Content from '../feedScreen/Content';
 import Footer from '../feedScreen/Footer';
 import Gap from '../../components/Gap';
-import { POST_VERB_POLL } from '../../utils/constants';
+import {POST_VERB_POLL} from '../../utils/constants';
 import ContentPoll from '../feedScreen/ContentPoll';
 
 const {width, height} = Dimensions.get('window');
-import {blockUser} from '../../service/blocking';
-import {showMessage} from 'react-native-flash-message';
 
 const PostDetailPage = (props) => {
   const [more, setMore] = useState(10);
@@ -143,10 +141,10 @@ const PostDetailPage = (props) => {
             />
           ) : (
             <Content
-            message={item.message}
-            images_url={item.images_url}
-            style={item.images_url.length > 0 ? {height: height * 0.5} : null}
-          />
+              message={item.message}
+              images_url={item.images_url}
+              style={item.images_url.length > 0 ? {height: height * 0.5} : null}
+            />
           )}
 
           <Gap style={{height: 16}} />
