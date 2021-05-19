b1236b957e8951ffe759db6564e91ae130da1c21
usupsuparma <usup.suparma@bijb.co.id>
2021-05-19 14:22:30 +0700

    fixing refresh token

--- src/service/users.js
+++ src/service/users.js
@@ -1,5 +1,11 @@
 import api from './config';
 import crashlytics from '@react-native-firebase/crashlytics';
+import {
+  getRefreshToken,
+  setAccessToken,
+  setRefreshToken,
+} from '../data/local/accessToken';
+import {BASE_URL} from '@env';
 export const verifyUser = async (userId) => {
   try {
     let resApi = await api.post('/users/verify-user', {
@@ -46,14 +52,44 @@ export const registerUser = async (data) => {
   }
 };
 
+const verifyAccessToken = async () => {
+  return api
+    .get('/users/veryfy-token-getstream', {})
+    .then((res) => {
+      return res.data;
+    })
+    .catch((err) => {
+      let code = err.response.status;
+      return code;
+    });
+};
+
 export const verifyTokenGetstream = async () => {
-  try {
-    let resApi = await api.get('/users/veryfy-token-getstream');
-    console.log('tokn very', resApi.data);
-    return resApi.data;
-  } catch (error) {
-    crashlytics().recordError(new Error(error));
-    console.log(error);
-    return null;
+  let status = await verifyAccessToken();
+  if (status === 401) {
+    let res = await refreshToken();
+    if (res.code === 200) {
+      await setAccessToken(res.data.token);
+      await setRefreshToken(res.data.refresh_token);
+      return res.data.token;
+    } else {
+      return null;
+    }
+  } else {
+    return status;
   }
+};
+
+export const refreshToken = async () => {
+  const token = await getRefreshToken();
+  const options = {
+    method: 'get',
+    headers: {
+      Authorization: 'Bearer ' + token,
+    },
+  };
+
+  // const resp = await fetchWithTimeout(this.url, options, 10000);
+  const resp = await fetch(BASE_URL + '/users/refresh-token', options);
+  return await resp.json();
 };
