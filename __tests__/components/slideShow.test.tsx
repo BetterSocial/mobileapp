a4496fd166ddc83f19586e55af6a61399e23bd2b
Ali Irawan <boylevantz@gmail.com>
2021-06-11 15:37:21 +0700

    adding mock for FlatList

--- __tests__/components/slideShow.test.tsx
+++ __tests__/components/slideShow.test.tsx
@@ -1,11 +1,48 @@
 import React from 'react';
-import {shallow, configure} from 'enzyme';
+import { View } from 'react-native';
+import { shallow, configure } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import renderer from 'react-test-renderer';
 import Adapter from 'enzyme-adapter-react-16';
 import SlideShow from '../../src/components/SignIn/SlideShow';
 
-configure({adapter: new Adapter()});
+jest.mock("react-native", () => {
+  const React = require('react');
+  const View = React.View;
+
+  function MockedFlatList(props) {
+    const items = props.data.map((item, index) => {
+      const key = props.keyExtractor(item, index);
+      return (
+        <View key={key}>
+          {props.renderItem({ item, index })}
+        </View>
+      );
+    });
+    return (
+      <View>
+        {items}
+      </View>
+    );
+  }
+  return {
+    Dimensions: {
+      get: (data: string) => {
+        if (data === 'window') {
+          return {
+            width: 200,
+            height: 200,
+          }
+        }
+      }
+    },
+    FlatList: {
+      get: () => MockedFlatList,
+    }
+  }
+});
+
+configure({ adapter: new Adapter() });
 describe('component SlideShow', () => {
   it('SlideShow  snapshot', () => {
     const component = shallow(<SlideShow />);
