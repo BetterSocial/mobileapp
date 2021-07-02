defdc6500332aaac191dfd0860190b8277dd0dcd
Putu Eka Mulyana <putuekamulyana97@gmail.com>
2021-07-02 19:14:26 +0800

    update test

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,7 +1,7 @@
 import ChannelListScreen from './channelListScreen/ChannelListScreen';
 import ChannelScreen from './channelScreen/ChannelListScreen';
 import FeedScreen from './feedScreen/FeedScreen';
-import NewsScreen from './newsScreen';
+import NewsScreen from './NewsScreen';
 import ProfileScreen from './ProfileScreen';
 
 export {
