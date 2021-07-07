d5fa6cf5b5e7f68e457e0844cc0815da27d7df65
usupsuparma <usup.suparma@bijb.co.id>
2021-07-07 22:16:31 +0700

    create group chat

---
+++ src/service/chat.js
@@ -0,0 +1,32 @@
+import {StreamChat} from 'stream-chat';
+import {STREAM_API_KEY} from '@env';
+import {getAccessToken} from '../utils/token';
+import jwtDecode from 'jwt-decode';
+const chatClient = new StreamChat(STREAM_API_KEY);
+const createChannel = async (
+  channelType,
+  members,
+  channelName,
+  image = null,
+) => {
+  try {
+    const token = await getAccessToken();
+    const id = await jwtDecode(token).user_id;
+    let user = {
+      id: id,
+    };
+    await chatClient.connectUser(user, token);
+    const channel = await chatClient.channel(channelType, {
+      members: members,
+      name: channelName,
+      //   image: null,
+    });
+    let res = await channel.create();
+    console.log(res);
+    return res;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export {createChannel};
