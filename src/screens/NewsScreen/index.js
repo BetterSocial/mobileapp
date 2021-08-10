b92680396c1ba4307df707510b3b596253eca39a
Putu Eka Mulyana <putuekamulyana97@gmail.com>
2021-08-10 10:29:54 +0800

    block domain in newsScreen

--- src/screens/NewsScreen/index.js
+++ src/screens/NewsScreen/index.js
@@ -1,13 +1,5 @@
 import * as React from 'react';
-import {
-  View,
-  Text,
-  StyleSheet,
-  FlatList,
-  Animated,
-  ScrollView,
-  PanResponder,
-} from 'react-native';
+import {View, StyleSheet, FlatList, Animated} from 'react-native';
 
 import analytics from '@react-native-firebase/analytics';
 import Toast from 'react-native-simple-toast';
@@ -21,15 +13,25 @@ import {getDomains} from '../../service/domain';
 import theme, {COLORS, FONTS, SIZES} from '../../utils/theme';
 import RenderItem from './RenderItem';
 import Search from './Search';
+import BlockDomain from '../../components/Blocking/BlockDomain';
+import SpecificIssue from '../../components/Blocking/SpecificIssue';
+import ReportDomain from '../../components/Blocking/ReportDomain';
+import {blockDomain} from '../../service/blocking';
 
 const NewsScreen = ({}) => {
   const navigation = useNavigation();
+  const blockDomainRef = React.useRef(null);
+  const refSpecificIssue = React.useRef(null);
+  const refReportDomain = React.useRef(null);
   const [data, setData] = React.useState([]);
   const offset = React.useRef(new Animated.Value(0)).current;
 
   const [loading, setLoading] = React.useState(false);
   const [yourselfId, setYourselfId] = React.useState('');
-
+  const [domain, setDomain] = React.useState('');
+  const [reportOption, setReportOption] = React.useState([]);
+  const [messageReport, setMessageReport] = React.useState('');
+  const [idBlock, setIdBlock] = React.useState('');
   let lastDragY = 0;
   React.useEffect(() => {
     const unsubscribe = navigation.addListener('tabPress', (e) => {
@@ -73,25 +75,24 @@ const NewsScreen = ({}) => {
   let handleScrollEvent = (event) => {
     let y = event.nativeEvent.contentOffset.y;
     let dy = y - lastDragY;
-    if (dy <= 0)
+    if (dy <= 0) {
       return Animated.timing(offset, {
         toValue: 0,
         duration: 50,
         useNativeDriver: false,
       }).start();
-    else if (dy > 0)
+    } else if (dy > 0) {
       return Animated.timing(offset, {
         toValue: -70,
         duration: 50,
         useNativeDriver: false,
       }).start();
+    }
   };
 
   let handleOnScrollBeginDrag = (event) => {
     lastDragY = event.nativeEvent.contentOffset.y;
   };
-
-  const goToScreenCreatePost = () => navigation.navigate('CreatePost');
 
   const shareNews = (news) => {};
 
@@ -99,28 +100,64 @@ const NewsScreen = ({}) => {
     navigation.navigate('DetailDomainScreen', {item: news});
   };
 
-  const blockNews = (news) => {};
+  const blockNews = (news) => {
+    blockDomainRef.current.open();
+    console.log('news ', news);
+    setIdBlock(news.content.domain_page_id);
+    setDomain(news.domain.name);
+  };
+  const selectBlock = (v) => {
+    if (v === 1) {
+      onBlockDomain();
+    } else {
+      refReportDomain.current.open();
+    }
+    blockDomainRef.current.close();
+  };
+  const getSpecificIssue = (v) => {
+    setMessageReport(v);
+    refSpecificIssue.current.close();
+    setTimeout(() => {
+      onBlockDomain();
+    }, 500);
+  };
+  const onSkipOnlyBlock = () => {
+    refReportDomain.current.close();
+    refSpecificIssue.current.close();
+    onBlockDomain();
+  };
+  const onNextQuestion = (v) => {
+    setReportOption(v);
+    refReportDomain.current.close();
+    refSpecificIssue.current.open();
+  };
+
+  const onBlockDomain = async () => {
+    const dataBlock = {
+      domainId: idBlock,
+      reason: reportOption,
+      message: messageReport,
+      source: 'domain_screen',
+    };
+    const result = await blockDomain(dataBlock);
+    if (result.code === 200) {
+      Toast.show(
+        'The user was blocked successfully. \nThanks for making BetterSocial better!',
+        Toast.LONG,
+      );
+    } else {
+      Toast.show('Your report was filed & will be investigated', Toast.LONG);
+    }
+    console.log('result block user ', result);
+  };
 
   const upvoteNews = async (news) => {
-    // console.log(news);
     upVoteDomain(news);
-    // if (result.code === 200) {
-    //   Toast.show('up vote was successful', Toast.LONG);
-    // } else {
-    //   Toast.show('up vote failed', Toast.LONG);
-    // }
   };
 
   const downvoteNews = async (news) => {
-    console.log(news);
     downVoteDomain(news);
-    // if (result.code === 200) {
-    //   Toast.show('down vote success', Toast.LONG);
-    // } else {
-    //   Toast.show('down vote failed', Toast.LONG);
-    // }
   };
-  console.log('news screen');
 
   return (
     <View style={styles.container}>
@@ -132,7 +169,9 @@ const NewsScreen = ({}) => {
           scrollEventThrottle={16}
           data={data}
           renderItem={({item, index}) => {
-            if (item.dummy) return <View key={index} style={{height: 68}} />;
+            if (item.dummy) {
+              return <View key={index} style={{height: 68}} />;
+            }
             return (
               <RenderItem
                 key={item}
@@ -148,6 +187,21 @@ const NewsScreen = ({}) => {
           }}
         />
 
+        <BlockDomain
+          refBlockDomain={blockDomainRef}
+          onSelect={selectBlock}
+          domain={domain}
+        />
+        <SpecificIssue
+          refSpecificIssue={refSpecificIssue}
+          onPress={getSpecificIssue}
+          onSkip={onSkipOnlyBlock}
+        />
+        <ReportDomain
+          refReportDomain={refReportDomain}
+          onSkip={onSkipOnlyBlock}
+          onSelect={onNextQuestion}
+        />
         <Loading visible={loading} />
       </Animated.View>
     </View>
@@ -156,7 +210,6 @@ const NewsScreen = ({}) => {
 
 const styles = StyleSheet.create({
   container: {
-    // paddingTop: SIZES.base,
     backgroundColor: COLORS.gray6,
   },
 });
