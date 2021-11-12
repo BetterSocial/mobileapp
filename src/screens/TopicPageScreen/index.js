01e7a5603f465d887a1eeebc902e2c4690f82808
usupsuparma <usup@solusiteknologi.co.id>
2021-11-12 23:01:37 +0700

    add screen

--- src/screens/TopicPageScreen/index.js
+++ src/screens/TopicPageScreen/index.js
@@ -6,6 +6,9 @@ const TopicPageScreen = () => {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Navigation domain={'Bali'} />
+      <View style={{ flex: 1 }}>
+        <Text>Not implement, in progress</Text>
+      </View>
     </View>
   );
 };
