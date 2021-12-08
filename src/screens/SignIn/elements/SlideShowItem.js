20bb80b32d2ae9a33b8f55690f9f1603b411f629
Muhammad Fajariansyah Ismail <fajariansyah.ismail@gmail.com>
2021-12-08 15:45:08 +0700

    new onboarding image and text

--- src/screens/SignIn/elements/SlideShowItem.js
+++ src/screens/SignIn/elements/SlideShowItem.js
@@ -1,7 +1,10 @@
 import * as React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { Dimensions, StyleSheet, Text, View } from 'react-native';
 
-export const SlideShowItem = ({index, children}) => {
+import { fonts } from '../../../utils/fonts';
+
+export const SlideShowItem = ({index, children, title, text, lineHeight = 24}) => {
+    const width = Dimensions.get('screen').width
     const getBackgroundColor = () => {
         switch(index) {
             case 0 : return '#FFDFA0'
@@ -14,14 +17,39 @@ export const SlideShowItem = ({index, children}) => {
 
     let backgroundColor = getBackgroundColor()
 
-    return <View style={styles.slideShowItemContainer}>
+    return <View style={styles.slideShowItemContainer(backgroundColor, width)}>
         {children}
+        <Text style={styles.title}>{title}</Text>
+        <Text style={styles.text(lineHeight)}>{text}</Text>
     </View>
 }
 
 const styles = StyleSheet.create({
-    slideShowItemContainer : {
-        justifyContent:'center',
-        flex: 1,
+    slideShowItemContainer : (backgroundColor, width) => {
+        return {
+            justifyContent:'center',
+            flex: 1,
+            backgroundColor : backgroundColor,
+            maxWidth: width,
+        }
     },
+    title: {
+        fontSize: 18,
+        lineHeight:12.5,
+        fontFamily: fonts.inter[600],
+        zIndex: 1000,
+        paddingTop: 18,
+        textAlign: 'center'
+    },
+    text: (lineHeight) => {
+        return {
+            fontSize: 15,
+            lineHeight:lineHeight,
+            marginHorizontal: 25,
+            marginTop: 20,
+            fontFamily: fonts.inter[500],
+            zIndex: 1000,
+            textAlign: 'center'
+        }
+    }
 });
