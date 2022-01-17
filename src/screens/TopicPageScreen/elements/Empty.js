7027f77786b11f0554a0955eecb8c172c63ef96b
usupsuparma <usup@solusiteknologi.co.id>
2022-01-17 19:16:24 +0700

    fixing empty state

--- src/screens/TopicPageScreen/elements/Empty.js
+++ src/screens/TopicPageScreen/elements/Empty.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {
   View, Text, Image, StyleSheet, Platform, Dimensions,
 } from 'react-native';
+import { Gap } from '../../../components';
+import { fonts } from '../../../utils/fonts';
 
 const Empty = () => (
   <View style={styles.container}>
@@ -9,6 +11,11 @@ const Empty = () => (
       style={styles.image}
       source={require('../../../assets/ic_empty.png')}
     />
+    <Gap height={20} />
+    <View style={{ width: 241, alignItems: 'center', justifyContent: 'center' }}>
+      <Text style={styles.text}>No posts yet - start this topic off by
+        posting about it</Text>
+    </View>
   </View>
 );
 
@@ -23,6 +30,14 @@ const styles = StyleSheet.create({
     width: 300,
     height: 300,
   },
+  text: {
+    fontSize: 14,
+    fontFamily: fonts.inter[400],
+    fontWeight: '400',
+    lineHeight: 16,
+    color: '#000000',
+    textAlign: 'center'
+  }
 });
 
 export default Empty;
