0428bb85f73e95377b99663344b3af97581fae04
agitafirstawan <agitafirstawan26@gmail.com>
2022-02-06 15:19:37 +0700

    add block and vote

--- src/screens/ChannelListScreen/elements/components/ListFeedNotification.js
+++ src/screens/ChannelListScreen/elements/components/ListFeedNotification.js
@@ -1,8 +1,14 @@
 import React, {memo} from 'react'
 import { View, Image, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import PropTypes from 'prop-types';
+import moment from 'moment'
+
 import { COLORS } from '../../../../utils/theme'
 import {Context} from '../../../../context';
+import MemoIc_arrow_down_vote_on from '../../../../assets/arrow/Ic_downvote_on';
+import MemoIc_arrow_upvote_on from '../../../../assets/arrow/Ic_upvote_on';
+import MemoIc_comment from '../../../../assets/icons/Ic_comment';
+import MemoIc_block_inactive from '../../../../assets/block/Ic_block_inactive';
 
 const styles = StyleSheet.create({
     containerCard: {
@@ -10,9 +16,7 @@ const styles = StyleSheet.create({
         backgroundColor: 'white',
         borderBottomColor: '#f2f2f2',
         borderBottomWidth: 2,
-        flexDirection: 'row',
-        alignItems: 'center',
-        paddingVertical: 10
+        paddingVertical: 5,
     },
     avatar: {
         height: 40,
@@ -32,6 +36,26 @@ const styles = StyleSheet.create({
     },
     subtitleStyle: {
         color: COLORS.gray
+    },
+    descriptionContainer: {
+        flexDirection: 'row',
+        marginTop: 5,
+        alignItems: 'center'
+    },
+    row: {
+        flexDirection: 'row'
+    },
+    avatarNoHeight: {
+        width: 40
+    },
+    centerAlign: {
+        alignItems: 'center'
+    },
+    mr10: {
+        marginRight: 15
+    },
+    textVoteMargin: {
+        marginRight: 5
     }
 })
 
@@ -40,27 +64,65 @@ const ListFeedNotification = ({notif}) => {
     const [user] = React.useContext(Context).users;
     const [profile] = React.useContext(Context).profile;
 
-    console.log(profile, user, 'sapilak')
+    const handleDate = (reaction) => {
+        if(reaction && reaction.created_at) {
+            console.log(reaction.created_at, 'jamil')
+            return moment(reaction.created_at).format('dddd')
+        }
+        return ""
+    }
+
+    console.log(notif, 'sapilak')
     return (
         <TouchableOpacity style={styles.containerCard} >
+            <View style={styles.row} >
             <View style={styles.avatarContainer} >
                 {user.photoUrl ? <Image source={{ uri: user.photoUrl }} style={styles.avatar} /> : null}
             </View>
             <View style={styles.titleContainer} >
-                <Text style={styles.titleText} >{profile.myProfile && profile.myProfile.username}'s post : {notif.activities[0] && notif.activities[0].object.message}</Text>
+                <Text style={styles.titleText} >{profile.myProfile && profile.myProfile.username}'s post : {notif.titlePost}</Text>
                 <Text style={styles.subtitleStyle} >
-                    {notif.activities[0] 
-                    && notif.activities[0].actor 
-                    && notif.activities[0].actor.data 
-                    && notif.activities[0].actor.data.username}
+                    {notif.comments[0] 
+                    && notif.comments[0].actor 
+                    && notif.comments[0].actor.data 
+                    && notif.comments[0].actor.data.username}
                     {" "}
-                    {notif.activities[0] 
-                    && notif.activities[0].reaction 
-                    && notif.activities[0].reaction.data 
-                    && notif.activities[0].reaction.data.text} </Text>
+                    {notif.comments[0] 
+                    && notif.comments[0].reaction 
+                    && notif.comments[0].reaction.data 
+                    && notif.comments[0].reaction.data.text} </Text>
             </View>
             <View style={styles.lastContentContainer} >
-                <Text>Thursday</Text>
+                <Text>{handleDate(notif.comments[0] && notif.comments[0].reaction)} </Text>
+            </View>
+            </View>
+         
+            <View style={[styles.descriptionContainer]} >
+                <View style={[styles.avatarContainer, styles.avatarNoHeight]} />
+                <View style={[styles.row, styles.centerAlign, styles.mr10]} >
+                    <Text style={styles.textVoteMargin} >
+                        {notif.upvote}
+                    </Text>
+                    <MemoIc_arrow_down_vote_on width={15} height={15} />
+                </View>
+                <View style={[styles.row, styles.centerAlign, styles.mr10]} >
+                    <Text style={styles.textVoteMargin} >
+                        {notif.downvote}
+                    </Text>
+                    <MemoIc_arrow_upvote_on width={15} height={15} />
+                </View>
+                <View style={[styles.row, styles.centerAlign, styles.mr10]} >
+                    <Text style={styles.textVoteMargin} >
+                        {notif.comments.length}
+                    </Text>
+                    <MemoIc_comment width={15} height={15} />
+                </View>
+                <View style={[styles.row, styles.centerAlign, styles.mr10]} >
+                    <Text style={styles.textVoteMargin} >
+                        {String(notif.block)}
+                    </Text>
+                    <MemoIc_block_inactive width={15} height={15} />
+                </View>
             </View>
         </TouchableOpacity>
     )
