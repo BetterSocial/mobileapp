28cee48d72dc19fed20dfc0f315bd8a9ed190f34
agitafirstawan <agitafirstawan26@gmail.com>
2022-03-11 08:51:13 +0700

    update metro

--- metro.config.js
+++ metro.config.js
@@ -1,23 +1,34 @@
- const { getDefaultConfig } = require('metro-config');
+const PATH = require('path');
+const blacklist = require('metro-config/src/defaults/exclusionList');
 
- module.exports = (async () => {
-   const {
-     resolver: { sourceExts, assetExts },
-   } = await getDefaultConfig();
- 
-   return {
-     transformer: {
-       babelTransformerPath: require.resolve('react-native-svg-transformer'),
-       getTransformOptions: async () => ({
-         transform: {
-           experimentalImportSupport: false,
-           inlineRequires: false,
-         },
-       }),
-     },
-     resolver: {
-       assetExts: assetExts.filter(ext => ext !== 'svg'),
-       sourceExts: [...sourceExts, 'svg'],
-     },
-   };
- })();
+const extractLinkedPackages = require('stream-chat-react-native-core/metro-dev-helpers/extract-linked-packages');
+
+const projectRoot = PATH.resolve(__dirname);
+const { alternateRoots, extraNodeModules, moduleBlacklist } = extractLinkedPackages(projectRoot);
+const { getDefaultConfig } = require('metro-config');
+
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts, assetExts },
+  } = await getDefaultConfig();
+
+  return {
+    transformer: {
+      babelTransformerPath: require.resolve('react-native-svg-transformer'),
+      getTransformOptions: async () => ({
+        transform: {
+          experimentalImportSupport: false,
+          inlineRequires: false,
+        },
+      }),
+    },
+    resolver: {
+      assetExts: assetExts.filter((ext) => ext !== 'svg'),
+      sourceExts: [...sourceExts, 'svg'],
+      extraNodeModules,
+      useWatchman: false,
+      blacklistRE: blacklist(moduleBlacklist),
+    },
+    watchFolders: [projectRoot].concat(alternateRoots),
+  };
+})();
