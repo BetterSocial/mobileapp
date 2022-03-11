a43988b414f836624df5e6a88de151cfe3223c07
agitafirstawan <agitafirstawan26@gmail.com>
2022-03-11 15:11:10 +0700

    fix metro config

--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,3 @@
-const PATH = require('path');
-const blacklist = require('metro-config/src/defaults/exclusionList');
-
-const extractLinkedPackages = require('stream-chat-react-native-core/metro-dev-helpers/extract-linked-packages');
-
-const projectRoot = PATH.resolve(__dirname);
-const { alternateRoots, extraNodeModules, moduleBlacklist } = extractLinkedPackages(projectRoot);
 const { getDefaultConfig } = require('metro-config');
 
 module.exports = (async () => {
@@ -25,10 +18,6 @@ module.exports = (async () => {
     resolver: {
       assetExts: assetExts.filter((ext) => ext !== 'svg'),
       sourceExts: [...sourceExts, 'svg'],
-      extraNodeModules,
-      useWatchman: false,
-      blacklistRE: blacklist(moduleBlacklist),
     },
-    watchFolders: [projectRoot].concat(alternateRoots),
   };
 })();
