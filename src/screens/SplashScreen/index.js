d2652b69d44d9e578706a90881f855b45907ecd3
agitafirstawan <agitafirstawan26@gmail.com>
2022-04-14 08:20:36 +0700

    fix splash screen animation

--- src/screens/SplashScreen/index.js
+++ src/screens/SplashScreen/index.js
@@ -8,50 +8,109 @@ import {
   StatusBar,
   StyleSheet,
 } from 'react-native';
-import {useNavigation} from '@react-navigation/core';
-
+import { useNavigation } from '@react-navigation/core';
+import { StackActions } from '@react-navigation/native';
+import { debounce } from 'lodash';
 import following from '../../context/actions/following';
 import { Context } from '../../context';
-import {getAccessToken} from '../../utils/token';
+import { getAccessToken } from '../../utils/token';
 import { getDomains, getFollowedDomain } from '../../service/domain';
-import {getFollowing, getProfileByUsername} from '../../service/profile';
+import { getFollowing, getProfileByUsername } from '../../service/profile';
 import { getFollowingTopic } from '../../service/topics';
-import {getUserId} from '../../utils/users';
+import { getUserId } from '../../utils/users';
 import { setNews } from '../../context/actions/news';
-import {useClientGetstream} from '../../utils/getstream/ClientGetStram';
-import {verifyTokenGetstream} from '../../service/users';
+import { useClientGetstream } from '../../utils/getstream/ClientGetStram';
+import { verifyTokenGetstream } from '../../service/users';
 
 const SplashScreen = () => {
   const navigation = useNavigation();
   const BASE_DEEPLINK_URL_REGEX = 'link.bettersocial.org';
-  let [isModalShown, setIsModalShown] = React.useState(false);
+  const [isModalShown, setIsModalShown] = React.useState(false);
 
-  const [, newsDispatch] = React.useContext(Context).news
-  const [, followingDispatch] = React.useContext(Context).following
-
+  const [, newsDispatch] = React.useContext(Context).news;
+  const [, followingDispatch] = React.useContext(Context).following;
   const create = useClientGetstream();
-  React.useEffect(() => {
-    analytics().logScreenView({
-      screen_class: 'SplashScreen',
-      screen_name: 'Splash Screen',
-    });
+  const debounceNavigationPage = debounce((selfUserId) => {
+    navigation.dispatch(StackActions.replace(selfUserId ? 'HomeTabs' : 'SignIn'));
+  }, 500);
 
-    getDeepLinkUrl();
-  }, []);
+  const getDiscoveryData = async (selfUserId) => {
+    if (!selfUserId) {
+      SplashScreenPackage.hide();
+      return debounceNavigationPage(selfUserId);
+    }
+    // Not using await so splash screen can navigate to next screen faster
 
-  let getDeepLinkUrl = async () => {
     try {
-      let selfUserId = await doVerifyUser();
-      let deepLinkUrl = await Linking.getInitialURL();
+      const followingUser = await getFollowing(selfUserId);
+      const domains = await getDomains();
+      const followedDomain = await getFollowedDomain();
+      const followingTopic = await getFollowingTopic();
+      await following.setFollowingUsers(followingUser.data, followingDispatch);
+      await setNews([{ dummy: true }, ...domains.data], newsDispatch);
+      await following.setFollowingDomain(followedDomain.data.data, followingDispatch);
+      await following.setFollowingTopics(followingTopic.data, followingDispatch);
+      SplashScreenPackage.hide();
+      debounceNavigationPage(selfUserId);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const navigateWithoutDeeplink = async (selfUserId) => {
+    await getDiscoveryData(selfUserId);
+  };
+
+  const doGetProfileByUsername = async (username) => {
+    try {
+      const response = await getProfileByUsername(username);
+      console.log('response.data');
+      console.log(response.data);
+      if (response.code === 200) {
+        console.log('response.data');
+        console.log(response.data);
+        return response.data;
+      }
+      return false;
+    } catch (e) {
+      return false;
+    }
+  };
+
+  const doVerifyUser = async () => {
+    try {
+      const token = await getAccessToken();
+      const id = await getUserId();
+      if (id !== null && id !== '') {
+        const verify = await verifyTokenGetstream();
+        if (verify !== null && verify !== '') {
+          create();
+          console.log('user id');
+          console.log(id);
+          return id;
+        }
+        return null;
+      }
+      return null;
+    } catch (e) {
+      console.log('doVerifyUser ', e);
+      return null;
+    }
+  };
+
+  const getDeepLinkUrl = async () => {
+    try {
+      const selfUserId = await doVerifyUser();
+      const deepLinkUrl = await Linking.getInitialURL();
       if (deepLinkUrl === null) {
         return navigateWithoutDeeplink(selfUserId);
       }
 
-      let match = deepLinkUrl.match(`(?<=${BASE_DEEPLINK_URL_REGEX}\/).+`);
+      const match = deepLinkUrl.match(`(?<=${BASE_DEEPLINK_URL_REGEX}\/).+`);
       if (match.length > 0) {
         setIsModalShown(true);
-        let username = match[0];
-        let otherProfile = await doGetProfileByUsername(username);
+        const username = match[0];
+        const otherProfile = await doGetProfileByUsername(username);
 
         if (!selfUserId || !otherProfile) {
           return navigateWithoutDeeplink(selfUserId);
@@ -64,8 +123,6 @@ const SplashScreen = () => {
           });
           return setIsModalShown(false);
         }
-
-        SplashScreenPackage.hide();
         // navigation.replace('OtherProfile', {
         //   data: {
         //     user_id: selfUserId,
@@ -81,101 +138,18 @@ const SplashScreen = () => {
     }
   };
 
-  let navigateWithoutDeeplink = async (selfUserId) => {
-    await SplashScreenPackage.hide();
-    await getDiscoveryData(selfUserId)
-    navigation.replace(selfUserId ? 'HomeTabs' : 'SignIn');
-    // navigation.replace(selfUserId ? 'DiscoveryScreen' : 'SignIn');
-  };
+  React.useEffect(() => {
+    analytics().logScreenView({
+      screen_class: 'SplashScreen',
+      screen_name: 'Splash Screen',
+    });
 
-  let doVerifyUser = async () => {
-    try {
-      let token = await getAccessToken();
-      let id = await getUserId();
-      if (id !== null && id !== '') {
-        const verify = await verifyTokenGetstream();
-        if (verify !== null && verify !== '') {
-          create();
-          console.log('user id');
-          console.log(id);
-          return id;
-        } else {
-          return null;
-        }
-      }
-      return null;
-    } catch (e) {
-      console.log('doVerifyUser ', e);
-      return null;
-    }
-  };
-
-  let doGetProfileByUsername = async (username) => {
-    try {
-      let response = await getProfileByUsername(username);
-      console.log('response.data')
-      console.log(response.data)
-      if (response.code === 200) {
-        console.log('response.data')
-        console.log(response.data)
-        return response.data;
-      }
-      return false;
-    } catch (e) {
-      return false;
-    }
-  };
-
-  const getDiscoveryData = async (selfUserId) => {
-    if(!selfUserId) return
-    // Not using await so splash screen can navigate to next screen faster
-    
-    try {
-      getFollowing(selfUserId).then((res) => {
-        following.setFollowingUsers(res.data, followingDispatch)
-      }) 
-
-      getDomains().then((res) => {
-        setNews([{ dummy: true}, ...res.data], newsDispatch)
-      }) 
-
-      getFollowedDomain().then((res) => {
-        following.setFollowingDomain(res.data.data, followingDispatch)
-      })
-
-      getFollowingTopic().then((res) => {
-        following.setFollowingTopics(res.data, followingDispatch)
-      })
-    } catch(e) {
-      console.log(e)
-    }
-    
-    return
-  }
+    getDeepLinkUrl();
+  }, []);
 
   return (
-    <SafeAreaView style={styles.container}>
-      <StatusBar translucent backgroundColor="transparent" />
-      <Image
-        style={styles.image}
-        source={require('../../assets/splash_screen.png')}
-      />
-    </SafeAreaView>
+    null
   );
 };
 
 export default SplashScreen;
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'white',
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  image: {
-    width: '100%',
-    height: '100%',
-    resizeMode: 'cover',
-  },
-});
