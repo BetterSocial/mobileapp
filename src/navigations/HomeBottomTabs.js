b410b67469dadd8f8f8ec7537ea339f9bd5dabbb
agitafirstawan <agita.firstawan@selleri.id>
2022-05-25 06:25:02 +0700

    fix: notification permission timing

--- src/navigations/HomeBottomTabs.js
+++ src/navigations/HomeBottomTabs.js
@@ -2,7 +2,9 @@ import * as React from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {StatusBar, StyleSheet, Text, View} from 'react-native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-
+import messaging from '@react-native-firebase/messaging';
+import PushNotification from 'react-native-push-notification';
+import PushNotificationIOS from '@react-native-community/push-notification-ios';
 import FirebaseConfig from '../configs/FirebaseConfig';
 import MemoFeed from '../assets/icon/Feed';
 import MemoHome from '../assets/icon/Home';
@@ -31,6 +33,43 @@ function HomeBottomTabs(props) {
   let [, dispatchProfile] = React.useContext(Context).profile;
   const [unReadMessage] = React.useContext(Context).unReadMessage;
   const [loadingUser, setLoadingUser] = React.useState(true)
+
+  PushNotification.configure({
+  // (required) Called when a remote is received or opened, or local notification is opened
+  onNotification(notification) {
+    console.log('NOTIFICATION:', notification);
+    // process the notification
+    // (required) Called when a remote is received or opened, or local notification is opened
+    notification.finish(PushNotificationIOS.FetchResult.NoData);
+  },
+
+  // (optional) Called when the user fails to register for remote notifications.
+  // Typically occurs when APNS is having issues, or the device is a simulator. (iOS)
+  onRegistrationError(err) {
+    console.error(err.message, err);
+  },
+  // IOS ONLY (optional): default: all - Permissions to register.
+  permissions: {
+    alert: true,
+    badge: true,
+    sound: true,
+  },
+  // Should the initial notification be popped automatically
+  // default: true
+  popInitialNotification: true,
+  requestPermissions: true,
+});
+
+  const requestPermission = async () => {
+    const authStatus = await messaging().requestPermission();
+    const enabled = authStatus === messaging.AuthorizationStatus.AUTHORIZED
+      || authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+    if (enabled) {
+      console.log('Authorization status:', authStatus);
+    }
+  };
+
   const getProfile = async () => {
     try {
       let selfUserId = await getUserId();
@@ -48,9 +87,55 @@ function HomeBottomTabs(props) {
       console.log(e);
     }
   };
+
+  const pushNotifIos = (message) => {
+    PushNotificationIOS.addNotificationRequest({
+      alertBody: message.notification.body,
+      alertTitle: message.notification.title
+    })
+  }
+
+  const pushNotifAndroid = (remoteMessage) => {
+    PushNotification.localNotification({
+      id: '123',
+      title: remoteMessage.notification.title,
+      channelId: 'bettersosialid',
+      message: remoteMessage.notification.body,
+    });
+  }
+
+  const createChannel = () => {
+    PushNotification.createChannel(
+      {
+        channelId: 'bettersosialid', // (required)
+        channelName: 'bettersosial-chat', // (required)
+        playSound: false, // (optional) default: true
+        soundName: 'default', // (optional) See `soundName` parameter of `localNotification` function
+        importance: 4, // (optional) default: 4. Int value of the Android notification importance
+        vibrate: true, // (optional) default: true. Creates the default vibration patten if true.
+      },
+      (created) => console.log(`createChannel returned '${created}'`), // (optional) callback returns whether the channel was created, false means it already existed.
+    );
+  };
+  
+
   React.useEffect(() => {
+    requestPermission()
     getProfile();
   }, []);
+
+  React.useEffect(() => {
+    createChannel()
+    const unsubscribe = messaging().onMessage((remoteMessage) => {
+      console.log('NOtifICAtion');
+      console.log('messag ', remoteMessage);
+      !isIos ? pushNotifAndroid(remoteMessage) : pushNotifIos(remoteMessage)
+   
+    });
+    return () => {
+      unsubscribe()
+    }
+  }, [])
 
   return (
     <SafeAreaView style={styles.container}>
