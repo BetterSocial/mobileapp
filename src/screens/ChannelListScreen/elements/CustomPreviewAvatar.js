cc3e53b83743808aea5ea4dfb7b185bffb9d7dc9
agitafirstawan <agita.firstawan@selleri.id>
2022-07-28 08:46:49 +0700

    fix: fix ping 1216

--- src/screens/ChannelListScreen/elements/CustomPreviewAvatar.js
+++ src/screens/ChannelListScreen/elements/CustomPreviewAvatar.js
@@ -3,7 +3,7 @@ import {
   ChannelAvatar,
   useChannelPreviewDisplayAvatar,
 } from 'stream-chat-react-native';
-import { Image, StyleSheet, View } from 'react-native';
+import { Image, StyleSheet, View, Text } from 'react-native';
 
 import DefaultChatGroupProfilePicture from '../../../assets/images/default-chat-group-picture.png';
 import { Context } from '../../../context';
@@ -16,45 +16,65 @@ const CustomPreviewAvatar = ({ channel }) => {
   React.useEffect(() => {
     setProfileChannel(dataChannel.images, dispatch);
   }, [dataChannel]);
-
   if (channel.data.channel_type === 2 || channel.data.channel_type === 3) {
     return (
+      <View style={styles.containerAvatar} >
       <Image
         source={{ uri: channel.data.image }}
         style={styles.image}
       />
+      <View style={styles.typeContainer} >
+          
+        </View>
+      </View>
     );
   }
 
   if (channel?.data?.image) {
     if (channel?.data?.image.indexOf('res.cloudinary.com') > -1) {
       return (
+        <View style={styles.containerAvatar} >
         <Image source={{uri: channel?.data?.image}} style={styles.image} />
+        <View style={styles.typeContainer} >
+          
+        </View>
+        </View>
       );
     }
 
     return (
+      <View style={styles.containerAvatar} >
       <Image
         source={{ uri: `data:image/jpg;base64,${channel?.data?.image}` }}
         style={styles.image}
       />
+      <View style={styles.typeContainer} >
+          
+        </View>
+      </View>
     );
-  } else if (getGroupMemberCount(channel) > 2) {
+  } if (getGroupMemberCount(channel) > 2) {
     return (
       <View style={styles.containerAvatar}>
         <Image
           source={DefaultChatGroupProfilePicture}
           style={styles.defaultGroupImage}
         />
+        <View style={styles.typeContainer} >
+          
+        </View>
       </View>
     );
-  } else {
+  } 
     return (
       <View style={styles.containerAvatar}>
         <ChannelAvatar channel={channel} />
+        <View style={styles.typeContainer} >
+          
+        </View>
       </View>
     );
-  }
+  
 };
 
 export default CustomPreviewAvatar;
@@ -64,7 +84,7 @@ const styles = StyleSheet.create({
     width: 40,
     height: 40,
     borderRadius: 20,
-    marginLeft: 8,
+    // marginLeft: 8,
   },
   defaultGroupImage: {
     width: 40,
@@ -75,4 +95,13 @@ const styles = StyleSheet.create({
   containerAvatar: {
     paddingLeft: 8,
   },
+    typeContainer: {
+    height: 20,
+    width: 20,
+    backgroundColor: 'red',
+    borderRadius: 10,
+    position: 'absolute',
+    bottom: -6,
+    right: 0
+  }
 });
