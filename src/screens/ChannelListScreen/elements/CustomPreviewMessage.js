93e23aa1a99eaeebfe1779e355a707ee15732c1c
agita firstawan <agita.firstawan@selleri.id>
2022-08-08 18:32:22 +0700

    feat: add username join initial

--- src/screens/ChannelListScreen/elements/CustomPreviewMessage.js
+++ src/screens/ChannelListScreen/elements/CustomPreviewMessage.js
@@ -1,9 +1,26 @@
 import React from "react";
+import { View, Text, StyleSheet } from "react-native";
 import {ChannelPreviewMessage} from 'stream-chat-react-native';
+
+const styles = StyleSheet.create({
+    message: {
+        flexShrink: 1,
+        fontSize: 12,
+      },
+})
 
 const PreviewMessage = (props) => {
     const {channel} = props
-    if(channel.data.channel_type === 2 || channel.data.channel_type === 3) return null
+    if(channel.data.channel_type === 2 || channel.data.channel_type === 3) return (
+        <Text numberOfLines={1} style={[styles.message, { color: '#7A7A7A' }]}>
+         <Text
+              style={[{ color: '#7A7A7A' }]}
+            >
+             {console.log(props, 'sulak')}
+             {props.latestMessagePreview.messageObject.user.name} has joined the group
+            </Text>
+      </Text>
+    )
     return (
         <ChannelPreviewMessage {...props} />
     )
