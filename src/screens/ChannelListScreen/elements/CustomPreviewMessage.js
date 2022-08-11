8bf745bf16d6acaed18a668c749974044c3ae03d
Muhammad Fajariansyah Ismail <fajar@Muhammads-MacBook-Pro.local>
2022-08-11 18:30:58 +0700

    feat: multiple poll calculation

--- src/screens/ChannelListScreen/elements/CustomPreviewMessage.js
+++ src/screens/ChannelListScreen/elements/CustomPreviewMessage.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
 import {ChannelPreviewMessage} from 'stream-chat-react-native';
+import { StyleSheet, Text, View } from "react-native";
 
 const styles = StyleSheet.create({
     message: {
@@ -11,7 +11,7 @@ const styles = StyleSheet.create({
 
 const PreviewMessage = (props) => {
     const {channel} = props
-    if(channel.data.channel_type === 2 || channel.data.channel_type === 3) return (
+    if(channel?.data?.channel_type === 2 || channel?.data?.channel_type === 3) return (
         <Text numberOfLines={1} style={[styles.message, { color: '#7A7A7A' }]}>
          <Text
               style={[{ color: '#7A7A7A' }]}
