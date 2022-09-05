3f3fea9c16342557c271ff8a8c5524bcabe2ca4b
Muhammad Fajariansyah Ismail <fajar@Muhammads-MacBook-Pro.local>
2022-09-05 16:17:59 +0700

    chore: add more test to remove prefix topic

--- src/utils/topics/removePrefixTopic.test.js
+++ src/utils/topics/removePrefixTopic.test.js
@@ -1,9 +1,13 @@
-import { PREFIX_TOPIC } from "../constants";
 import removePrefixTopic from "./removePrefixTopic";
+import { PREFIX_TOPIC } from "../constants";
 
 // eslint-disable-next-line no-undef
 describe("Topic test", () => {
     // eslint-disable-next-line no-undef
+    it('return undefined if string is null', () => {
+        expect(removePrefixTopic(null)).toBe(undefined)
+    }) 
+
     it('remove topic has prefix topic', () => {
         const topicWithPrefix = `${PREFIX_TOPIC}bali`;
         // eslint-disable-next-line no-undef
