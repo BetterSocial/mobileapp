9cee182fdb987bfea120e0b6f7a510b0ad7f57ba
agita firstawan <agitafirstawan26@gmail.com>
2022-09-06 16:54:54 +0700

    test: channel list test

---
+++ __tests__/Sceens/ChannelListScreen/ChannelListScreen.test.js
@@ -0,0 +1,24 @@
+
+// import React from 'react';
+// import { render, screen, fireEvent } from '@testing-library/react-native';
+// import ChannelList from '../../../src/screens/ChannelListScreen'
+
+// jest.mock('stream-chat-react-native', () => jest.fn())
+// jest.mock('stream-chat-react-native-core', () => jest.fn())
+// jest.mock('react-native-share', () => ({
+//     FACEBOOK: jest.fn()
+// }))
+// jest.mock('react-native-redash', () => jest.fn());
+
+// jest.mock('React', () => ({
+//     useRef: {
+//         current: jest.fn()
+//     }
+// }))
+
+describe('screen channel should same as snapshot', () => {
+    it('code should not change', () => {
+        // const container = render(<ChannelList />) 
+        // expect(container).toMatchSnapshot()       
+    })
+})
