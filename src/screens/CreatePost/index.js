67148c9590fd23b064ad2f76f909d6bd09587aec
Muhammad Fajariansyah Ismail <fajar@Muhammads-MacBook-Pro.local>
2022-09-27 22:23:22 +0700

    fix: back button

--- src/screens/CreatePost/index.js
+++ src/screens/CreatePost/index.js
@@ -1,7 +1,9 @@
-import * as React from 'react';
-import PSL from 'psl'
-import Toast from 'react-native-simple-toast';
 import analytics from '@react-native-firebase/analytics';
+import { useNavigation } from '@react-navigation/core';
+import { getLinkPreview } from 'link-preview-js';
+import { debounce, set } from 'lodash';
+import PSL from 'psl'
+import * as React from 'react';
 import {
     Alert,
     BackHandler,
@@ -16,46 +18,38 @@ import {
     TouchableNativeFeedback,
     View
 } from 'react-native';
-import { OpenGraphParser } from 'react-native-opengraph-kit'
-import { debounce, set } from 'lodash';
-import { getLinkPreview } from 'link-preview-js';
-import { launchCamera, launchImageLibrary } from 'react-native-image-picker';
 import { showMessage } from 'react-native-flash-message';
-import { useNavigation } from '@react-navigation/core';
+import { launchCamera, launchImageLibrary } from 'react-native-image-picker';
+import { OpenGraphParser } from 'react-native-opengraph-kit'
+import Toast from 'react-native-simple-toast';
 
-import Card from './elements/Card';
-import ContentLink from './elements/ContentLink';
-import CreatePollContainer from './elements/CreatePollContainer';
-import Gap from '../../components/Gap';
-import Header from '../../components/Header';
-import ListItem from '../../components/MenuPostItem';
-import Loading from '../Loading';
-import Location from '../../assets/icons/Ic_location';
 import MemoIc_hastag from '../../assets/icons/Ic_hastag';
+import Location from '../../assets/icons/Ic_location';
+import Timer from '../../assets/icons/Ic_timer';
 import MemoIc_user_group from '../../assets/icons/Ic_user_group';
 import MemoIc_world from '../../assets/icons/Ic_world';
 import ProfileDefault from '../../assets/images/ProfileDefault.png';
-import SheetAddTopic from './elements/SheetAddTopic';
-import SheetCloseBtn from './elements/SheetCloseBtn';
-import SheetExpiredPost from './elements/SheetExpiredPost';
-import SheetGeographic from './elements/SheetGeographic';
-import SheetMedia from './elements/SheetMedia';
-import SheetPrivacy from './elements/SheetPrivacy';
-import ShowMedia from './elements/ShowMedia';
-import StringConstant from '../../utils/string/StringConstant';
-import Timer from '../../assets/icons/Ic_timer';
-import TopicItem from '../../components/TopicItem';
-import UserProfile from './elements/UserProfile';
-import handleHastag from '../../utils/hastag';
 import { Button, ButtonAddMedia } from '../../components/Button';
+import Gap from '../../components/Gap';
+import Header from '../../components/Header';
+import ListItem from '../../components/MenuPostItem';
+import TopicItem from '../../components/TopicItem';
 import { Context } from '../../context';
-import { MAX_POLLING_ALLOWED, MIN_POLLING_ALLOWED } from '../../utils/constants';
-import { PROFILE_CACHE } from '../../utils/cache/constant';
+import { getUserForTagging } from '../../service/mention';
 import { ShowingAudience, createPollPost, createPost } from '../../service/post';
-import { capitalizeFirstText, convertString } from '../../utils/string/StringUtils';
+import { getMyProfile } from '../../service/profile';
+import { getTopics } from '../../service/topics';
+import { insertNewTopicIntoTopics } from '../../utils/array/ChunkArray';
+import { getSpecificCache } from '../../utils/cache';
+import { PROFILE_CACHE } from '../../utils/cache/constant';
 import { colors } from '../../utils/colors';
+import { MAX_POLLING_ALLOWED, MIN_POLLING_ALLOWED } from '../../utils/constants';
 import { fonts } from '../../utils/fonts';
-import { getDomainInfoInLinkPreview, getNewsLinkInfoInLinkPreview, getUrl, isContainUrl, isEmptyOrSpaces } from '../../utils/Utils';
+import handleHastag from '../../utils/hastag';
+import {
+    requestCameraPermission,
+    requestExternalStoragePermission,
+} from '../../utils/permission';
 import {
     getDurationId,
     getLocationId,
@@ -64,17 +58,23 @@ import {
     setLocationId,
     setPrivacyId,
 } from '../../utils/setting';
-import { getMyProfile } from '../../service/profile';
-import { getSpecificCache } from '../../utils/cache';
-import { getTopics } from '../../service/topics';
+import StringConstant from '../../utils/string/StringConstant';
+import { capitalizeFirstText, convertString } from '../../utils/string/StringUtils';
 import { getUserId } from '../../utils/users';
-import { insertNewTopicIntoTopics } from '../../utils/array/ChunkArray';
-import {
-    requestCameraPermission,
-    requestExternalStoragePermission,
-} from '../../utils/permission';
-import { getUserForTagging } from '../../service/mention';
+import { getDomainInfoInLinkPreview, getNewsLinkInfoInLinkPreview, getUrl, isContainUrl, isEmptyOrSpaces } from '../../utils/Utils';
+import Loading from '../Loading';
+import Card from './elements/Card';
+import ContentLink from './elements/ContentLink';
+import CreatePollContainer from './elements/CreatePollContainer';
+import SheetAddTopic from './elements/SheetAddTopic';
+import SheetCloseBtn from './elements/SheetCloseBtn';
+import SheetExpiredPost from './elements/SheetExpiredPost';
+import SheetGeographic from './elements/SheetGeographic';
+import SheetMedia from './elements/SheetMedia';
+import SheetPrivacy from './elements/SheetPrivacy';
+import ShowMedia from './elements/ShowMedia';
 import useHastagMention from './elements/useHastagMention';
+import UserProfile from './elements/UserProfile';
 
 const MemoShowMedia = React.memo(ShowMedia, compire);
 function compire(prevProps, nextProps) {
@@ -443,6 +443,7 @@ const CreatePost = () => {
             sheetBackRef.current.open();
             return true;
         }
+        
         navigation.goBack();
         return true;
     };
