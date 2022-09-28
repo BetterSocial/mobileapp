45b0d65833f4d4a917d83a40ea1fc8ee77332cd8
agita firstawan <agitafirstawan26@gmail.com>
2022-09-29 06:21:27 +0700

    fix: fix code ping 1320

--- src/screens/SignInV2/hooks/useSignin.js
+++ src/screens/SignInV2/hooks/useSignin.js
@@ -6,7 +6,7 @@ const useSignin = () => {
     const topicMapping = (data) => {
         const allTopics = []
         if(data && typeof data ==='object') {
-            Object.keys(data).map((attribute) => allTopics.push({name: attribute, data: data[attribute].map((att) => ({topic_id: att.topic_id, name: att.name}))}))
+            Object.keys(data).forEach((attribute) => allTopics.push({name: attribute, data: data[attribute].map((att) => ({topic_id: att.topic_id, name: att.name}))}))
         }
         saveToCache(TOPICS_PICK, allTopics)
     }
