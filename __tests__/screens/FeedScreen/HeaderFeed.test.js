1cc2ff7929fae8b01b1da74ee8ff22826e8ad2ac
Agita firstawan <36707967+agit26milan@users.noreply.github.com>
2022-12-01 14:11:52 +0700

    test: feed header test

--- __tests__/screens/FeedScreen/HeaderFeed.test.js
+++ __tests__/screens/FeedScreen/HeaderFeed.test.js
@@ -1,3 +1,22 @@
+import * as React from 'react'
+import {render, cleanup, fireEvent} from '@testing-library/react-native'
+import Header from '../../../src/screens/FeedScreen/Header'
+import Store from '../../../src/context/Store'
+
+jest.mock('react-native-activity-feed/node_modules/react-native-image-crop-picker', () => ({
+    openPicker: () => jest.fn()
+}))
+
 describe('Header feed should run correctly', () => {
-    it('code should not change', () => {})
+    it('code should not change', () => {
+        const {toJSON} = render(<Header props={{anonimity: false}}  />, {wrapper: Store})
+        expect(toJSON).toMatchSnapshot()
+    })
+
+    it('user normal should choose correct component', () => {
+        const {getAllByTestId} = render(<Header props={{anonimity: false}}  />, {wrapper: Store})
+        expect(getAllByTestId('defaultHeader')).toHaveLength(1)
+
+    })
+
 })
