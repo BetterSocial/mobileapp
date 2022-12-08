4de2a595b62113b506d14bbaf4dee1437099b14d
Agita firstawan <36707967+agit26milan@users.noreply.github.com>
2022-12-08 17:45:14 +0700

    test: test topic chip

---
+++ __tests__/components/TopicChip/TopicChip.test.js
@@ -0,0 +1,31 @@
+
+
+import * as React from 'react';
+import { render, cleanup, fireEvent } from '@testing-library/react-native';
+
+import TopicChip from '../../../src/components/TopicsChip/TopicsChip'
+
+const mockedNavigate = jest.fn()
+jest.mock('@react-navigation/native', () => ({
+    ...jest.requireActual('@react-navigation/native'),
+    useNavigation: () => ({
+      navigate: mockedNavigate,
+    }),
+  }));
+
+describe('It should be run correctly', () => {
+    const topics = ['#barusaja', '#yogyakarta']
+
+    afterEach(cleanup)
+
+    it('should match snapshot', () => {
+        expect(toJSON).toMatchSnapshot()
+        const {toJSON} = render(<TopicChip topics={topics} />)
+    })
+
+    it('onPress chip should open navigation', () => {
+        const {getAllByTestId} = render(<TopicChip topics={topics} />)
+        fireEvent.press(getAllByTestId('topic-chip')[0])
+        expect(mockedNavigate).toHaveBeenCalled()
+    })
+})
