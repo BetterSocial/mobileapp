0fad75a488b4abef303930252daea1ed2dec9c59
Agita firstawan <36707967+agit26milan@users.noreply.github.com>
2022-12-09 21:15:44 +0700

    test: pdp test

---
+++ __tests__/components/PDP/index.test.js
@@ -0,0 +1,62 @@
+import * as React from 'react';
+import { render, cleanup } from '@testing-library/react-native';
+import PDP from '../../../src/components/PostPageDetail';
+import Store from '../../../src/context/Store';
+
+
+jest.mock('react-native-activity-feed/node_modules/react-native-image-crop-picker', () => ({
+    openPicker: jest.fn()
+}))
+
+jest.mock('../../../src/hooks/useAfterInteractions', () => ({
+    useAfterInteractions: () => ({
+        transitionRef: {current: {animateNextTransition: jest.fn()}}, interactionsComplete: true
+    })
+}))
+
+const mockedGoBack = jest.fn()
+
+jest.mock('@react-navigation/native', () => ({
+    ...jest.requireActual('@react-navigation/native'),
+    useRoute: () => ({
+        params: {
+            isCaching: jest.fn().mockImplementation(true)
+        }
+    }),
+    useNavigation: () => ({
+        goBack: mockedGoBack
+    }),
+}))
+
+describe('PDP component should run correctly', () => {
+    const mockDispatch = jest.fn()
+    const props = {
+        dispatch: mockDispatch,
+        feedId: '9f7f7e87-7641-11ed-82ab-124f97b82f95',
+        feeds: [
+            {anonimity: false,
+            count_downvote: 0,
+            count_upvote: 0,
+            duration_feed: "1",
+            expired_at: "2022-12-08T15:12:53.000Z",
+            final_score: 0,
+            foreign_id: "",
+            id: "9f7f7e87-7641-11ed-82ab-124f97b82f95",
+            location: "Everywhere",
+            message: "#dia #dian #diam ",
+            object: "{\"feed_group\":\"main_feed\",\"message\":\"#dia #dian #diam \",\"profile_pic_path\":\"https://res.cloudinary.com/hpjivutj2/image/upload/v1660620884/nrfnzuhcrozz9v34ngv3.jpg\",\"real_name\":null,\"topics\":[\"dia\",\"dian\"],\"username\":\"Fajarism\",\"verb\":\"tweet\"}",
+            origin: null
+            }
+        ],
+        navigateToReplyView: jest.fn(),
+        page: "PostDetailPage",
+        setFeedByIndexProps: jest.fn()
+    }
+
+    afterEach(cleanup)
+
+    it('sould match snapshot', () => {
+        const {toJSON} = render(<PDP {...props} />, {wrapper: Store})
+        expect(toJSON).toMatchSnapshot()
+    })
+})
---
+++ __tests__/screens/PDP/PDP.test.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import {render, cleanup, fireEvent} from '@testing-library/react-native'
+import PDP from '../../../src/screens/PostPageDetail'
+import Store from '../../../src/context/Store'
+
+
+jest.mock('react-native-activity-feed/node_modules/react-native-image-crop-picker', () => ({
+    openPicker: jest.fn()
+}))
+
+jest.mock('../../../src/hooks/useAfterInteractions', () => ({
+    useAfterInteractions: () => ({
+        transitionRef: {current: {animateNextTransition: jest.fn()}}, interactionsComplete: true
+    })
+}))
+const mockedGoBack = jest.fn()
+jest.mock('@react-navigation/native', () => ({
+    ...jest.requireActual('@react-navigation/native'),
+    useRoute: () => ({
+        params: {
+            isCaching: jest.fn().mockImplementation(true)
+        }
+    }),
+    useNavigation: () => ({
+        goBack: mockedGoBack
+    }),
+}))
+
+describe('PDP page should run correctly', () => {
+    const props = {
+        route: {
+            params: {
+                isCaching: false,
+                refreshCache: jest.fn()
+            }
+        }
+    }
+
+    afterEach(cleanup)
+
+    it('should be same as snapshot', () => {
+        const {toJSON} = render(<PDP {...props} />, {wrapper: Store})
+        expect(toJSON).toMatchSnapshot()
+    })
+
+    it('refresh cache should call if refreshCache params exist', () => {
+        expect(props.route.params.refreshCache).toHaveBeenCalled()
+    })
+})
