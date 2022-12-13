e3307ce23e17950dc165a54026ff026f8e0375d3
Agita firstawan <36707967+agit26milan@users.noreply.github.com>
2022-12-13 16:06:54 +0700

    fix: fix remove unused import

--- src/components/TopicsChip/TopicsChip.js
+++ src/components/TopicsChip/TopicsChip.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Pressable, StyleSheet, Text, View, TouchableOpacity } from "react-native"
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native"
 import { useNavigation } from '@react-navigation/native';
 
 import { colors } from "../../utils/colors"
