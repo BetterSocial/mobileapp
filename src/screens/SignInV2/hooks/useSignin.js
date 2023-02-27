4e6777988c7c5d9bb0bc2e5e35c68266492838b6
Agita firstawan <36707967+agit26milan@users.noreply.github.com>
2023-02-27 09:33:42 +0700

    fix: fix issue on boarding topic

--- src/screens/SignInV2/hooks/useSignin.js
+++ src/screens/SignInV2/hooks/useSignin.js
@@ -1,33 +1,42 @@
+import React from 'react';
 import {get} from '../../../api/server';
-import {saveToCache} from '../../../utils/cache'
-import { TOPICS_PICK } from '../../../utils/cache/constant';
+import {saveToCache} from '../../../utils/cache';
+import {TOPICS_PICK} from '../../../utils/cache/constant';
 
 const useSignin = () => {
-    const topicMapping = (data) => {
-        const allTopics = []
-        if(data && typeof data ==='object') {
-            Object.keys(data).forEach((attribute) => allTopics.push({name: attribute, data: data[attribute].map((att) => ({topic_id: att.topic_id, name: att.name}))}))
-        }
-        saveToCache(TOPICS_PICK, allTopics)
+  const [topicCollection, setTopics] = React.useState([]);
+  const topicMapping = (data) => {
+    const allTopics = [];
+    if (data && typeof data === 'object') {
+      Object.keys(data).forEach((attribute) =>
+        allTopics.push({
+          name: attribute,
+          data: data[attribute].map((att) => ({
+            topic_id: att.topic_id,
+            name: att.name.toLowerCase()
+          }))
+        })
+      );
     }
+    setTopics(allTopics);
+    saveToCache(TOPICS_PICK, allTopics);
+  };
 
-    const getTopicsData = () => {
-        get({url: '/topics/list'}).then((res) => {
+  const getTopicsData = () => {
+    get({url: '/topics/list'})
+      .then((res) => {
         if (res.status === 200) {
-          topicMapping(res.data.body)
+          topicMapping(res.data.body);
         }
       })
       .catch((e) => {
         if (__DEV__) {
-          console.log('topics error: ', e)
+          console.log('topics error: ', e);
         }
       });
-    }
+  };
 
+  return {getTopicsData, topicCollection};
+};
 
-
-    return {getTopicsData}
-}
-
-
-export default useSignin
+export default useSignin;
