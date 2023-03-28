7ff67491394fa498989fb593ff55fac611464c9f
Muhammad Fajariansyah Ismail <fajarism@RokaM1.local>
2023-03-28 13:05:31 +0700

    feat: self anonymous feeds on profile

---
+++ src/hooks/screen/useProfileScreenHook.js
@@ -0,0 +1,54 @@
+import * as React from 'react';
+
+import ProfileRepo from '../../service/repo/profileRepo';
+import useMyProfileFeedContextHook from '../context/useMyProfileFeedContext';
+import {useAfterInteractions} from '../useAfterInteractions';
+
+export const TAB_INDEX_SIGNED = 0;
+export const TAB_INDEX_ANONYMOUS = 1;
+
+const useProfileScreenHook = () => {
+  const [profileTabIndex, setProfileTabIndex] = React.useState(TAB_INDEX_SIGNED);
+  const [isLoadingFetchingAnonymousPosts, setIsLoadingFetchingAnonymousPosts] =
+    React.useState(false);
+  const [isLoadingFetchingSignedPosts] = React.useState(false);
+
+  const {feeds, anonymousFeeds, setMyProfileAnonymousFeed} = useMyProfileFeedContextHook();
+  const {interactionsComplete} = useAfterInteractions();
+
+  const isProfileTabSigned = profileTabIndex === TAB_INDEX_SIGNED;
+
+  const setTabIndexToSigned = () => setProfileTabIndex(TAB_INDEX_SIGNED);
+
+  const setTabIndexToAnonymous = () => setProfileTabIndex(TAB_INDEX_ANONYMOUS);
+
+  const fetchAnonymousPost = async (offset = 0, limit = 10) => {
+    setIsLoadingFetchingAnonymousPosts(true);
+    const response = await ProfileRepo.getSelfAnonymousFeed(offset, limit);
+    setMyProfileAnonymousFeed(response?.data);
+    setIsLoadingFetchingAnonymousPosts(false);
+  };
+
+  const reloadFetchAnonymousPost = async () => {
+    fetchAnonymousPost();
+  };
+
+  React.useEffect(() => {
+    if (interactionsComplete) {
+      fetchAnonymousPost();
+    }
+  }, []);
+
+  return {
+    isLoadingFetchingAnonymousPosts,
+    isLoadingFetchingSignedPosts,
+    feeds: isProfileTabSigned ? feeds : anonymousFeeds,
+    setTabIndexToAnonymous,
+    setTabIndexToSigned,
+    reloadFetchAnonymousPost,
+    fetchAnonymousPost,
+    profileTabIndex
+  };
+};
+
+export default useProfileScreenHook;
