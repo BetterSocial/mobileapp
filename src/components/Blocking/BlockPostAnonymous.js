5fc62f159c8730abe3a0f7fb43b05751343049c6
Muhammad Fajariansyah Ismail <fajarism@RokaM1.local>
2023-03-31 14:11:37 +0700

    feat: block users v2

--- src/components/Blocking/BlockPostAnonymous.js
+++ src/components/Blocking/BlockPostAnonymous.js
@@ -1,9 +1,8 @@
 import * as React from 'react';
+import RBSheet from 'react-native-raw-bottom-sheet';
 import {StyleSheet, Text, View} from 'react-native';
 
-import RBSheet from 'react-native-raw-bottom-sheet';
-
-import ItemListLarge from '../../components/Blocking/ItemListLarge';
+import ItemListLarge from './ItemListLarge';
 import {colors} from '../../utils/colors';
 import {fonts} from '../../utils/fonts';
 
@@ -14,15 +13,15 @@ const BlockPostAnonymous = ({refBlockPostAnonymous, onSelect}) => {
       icon: 'block',
       label: "Block user's anonymous post indefinitely",
       desc: 'You won’t see any anonymous posts from this user and the user’s reach will be reduced across BetterSocial, in particular for this post. This action cannot be reversed!',
-      iconReght: false,
+      iconReght: false
     },
     {
       id: 2,
       icon: 'handcuffs',
       label: 'Block & report this user',
       desc: 'This account is fake, hacked, or involved in criminal activity. You will need to provide additional information to help us take consequences.',
-      iconReght: true,
-    },
+      iconReght: true
+    }
   ];
   return (
     <View>
@@ -32,14 +31,15 @@ const BlockPostAnonymous = ({refBlockPostAnonymous, onSelect}) => {
         closeOnPressMask={true}
         customStyles={{
           container: styles.container,
-          draggableIcon: styles.draggableIcon,
+          draggableIcon: styles.draggableIcon
         }}>
         <Text style={styles.title}>What do you want to do?</Text>
         <Text style={styles.desc}>
-          Blocking a post will not just remove their post from your feed, but
-          also reduce their visibility for all other posts.
+          {
+            "Blocking all of this user's anonymous post from your feed will also reduce their visibility for all other posts."
+          }
         </Text>
-        {data.map((item, index) => (
+        {data.map((item) => (
           <ItemListLarge
             key={item.id}
             id={item.id}
@@ -62,7 +62,7 @@ const styles = StyleSheet.create({
     fontFamily: fonts.inter[600],
     fontSize: 18,
     color: '#000',
-    marginLeft: 21,
+    marginLeft: 21
   },
   desc: {
     color: colors.gray,
@@ -70,15 +70,15 @@ const styles = StyleSheet.create({
     fontSize: 12,
     marginHorizontal: 21,
     marginTop: 17,
-    marginBottom: 29,
+    marginBottom: 29
   },
   container: {
     height: 'auto',
     borderTopRightRadius: 20,
-    borderTopLeftRadius: 20,
+    borderTopLeftRadius: 20
   },
   draggableIcon: {
     backgroundColor: colors.alto,
-    width: 60,
-  },
+    width: 60
+  }
 });
