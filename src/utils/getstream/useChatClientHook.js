251af185e58c508201a6ed1adc637be8a716e26c
agitafirstawan26@gmail.com <agitafirstawan26@gmail.com>
2023-04-01 09:55:33 +0700

    fix: ping 1734 check the latest message if have message

--- src/utils/getstream/useChatClientHook.js
+++ src/utils/getstream/useChatClientHook.js
@@ -1,52 +1,60 @@
 import * as React from 'react';
 
-import { Context } from '../../context';
-import { DEFAULT_TOPIC_PIC_PATH } from '../constants';
-import { putUserTopic } from '../../service/topics';
+import {Context} from '../../context';
+import {DEFAULT_TOPIC_PIC_PATH} from '../constants';
+import {putUserTopic} from '../../service/topics';
 
 const useChatClientHook = () => {
-    const [client] = React.useContext(Context).client
-    const [user] = React.useContext(Context).profile
-    const defaultImage = DEFAULT_TOPIC_PIC_PATH
+  const [client] = React.useContext(Context).client;
+  const [user] = React.useContext(Context).profile;
+  const defaultImage = DEFAULT_TOPIC_PIC_PATH;
 
-    const addTopicToChatTab = async (topic) => {
-        const channel = await client.client.channel('topics', `topic_${topic}`, {
-            name: `#${topic}`,
-            members: [user.myProfile.user_id],
-            channel_type: 3,
-            channel_image: defaultImage,
-            channelImage: defaultImage,
-            image: defaultImage
-        })
-        await channel.create()
-        await channel.addMembers([user.myProfile.user_id])
-        await channel.sendMessage({ text: `${user?.myProfile?.username} followed this topic` }, { skip_push: true })
+  const addTopicToChatTab = async (topic) => {
+    const filter = {id: {$eq: `topic_${topic}`}};
+
+    const queryChannel = await client.client.queryChannels(filter);
+    let text = '';
+    if (queryChannel.length > 0) {
+      const filterMessage = queryChannel[0].state.messages.filter((message) => message.text !== '');
+      text = filterMessage[filterMessage.length - 1].text;
+    }
+    const channel = await client.client.channel('topics', `topic_${topic}`, {
+      name: `#${topic}`,
+      members: [user.myProfile.user_id],
+      channel_type: 3,
+      channel_image: defaultImage,
+      channelImage: defaultImage,
+      image: defaultImage
+    });
+    await channel.create();
+    await channel.addMembers([user.myProfile.user_id]);
+    await channel.sendMessage({text}, {skip_push: true});
+  };
+
+  const removeTopicFromChatTab = async (topic) => {
+    const channel = await client.client.channel('topics', `topic_${topic}`);
+
+    await channel.removeMembers([user?.myProfile?.user_id]);
+  };
+
+  const followTopic = async (topic) => {
+    const data = {
+      name: topic
+    };
+
+    const response = await putUserTopic(data);
+    if (response?.data) {
+      addTopicToChatTab(topic);
+    } else {
+      removeTopicFromChatTab(topic);
     }
 
-    const removeTopicFromChatTab = async (topic) => {
-        const channel = await client.client.channel('topics', `topic_${topic}`)
+    return response?.data;
+  };
 
-        await channel.removeMembers([user?.myProfile?.user_id])
-    }
+  return {
+    followTopic
+  };
+};
 
-    const followTopic = async (topic) => {
-        const data = {
-            name: topic
-        }
-
-        const response = await putUserTopic(data);
-        if (response?.data) {
-            addTopicToChatTab(topic)
-        } else {
-            removeTopicFromChatTab(topic)
-        }
-
-        return response?.data
-    }
-
-    return {
-        followTopic
-    }
-}
-
-export default useChatClientHook
+export default useChatClientHook;
