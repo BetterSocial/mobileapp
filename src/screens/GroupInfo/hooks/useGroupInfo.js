56f7a4fff7cc63f81fab4c46adc75e22af24848b
agitafirstawan26@gmail.com <agitafirstawan26@gmail.com>
2023-04-19 21:57:02 +0700

    fix: dix adjus system message

--- src/screens/GroupInfo/hooks/useGroupInfo.js
+++ src/screens/GroupInfo/hooks/useGroupInfo.js
@@ -3,13 +3,13 @@ import {useNavigation} from '@react-navigation/core';
 
 import {launchImageLibrary} from 'react-native-image-picker';
 import {Alert} from 'react-native';
+import {generateRandomId} from 'stream-chat-react-native-core';
 import {Context} from '../../../context';
 import {setParticipants} from '../../../context/actions/groupChat';
 import {uploadFile} from '../../../service/file';
 import {requestExternalStoragePermission} from '../../../utils/permission';
 import {getChatName} from '../../../utils/string/StringUtils';
 import {setChannel} from '../../../context/actions/setChannel';
-import {generateRandomId} from 'stream-chat-react-native-core';
 import {checkUserBlock} from '../../../service/profile';
 
 const useGroupInfo = () => {
@@ -189,11 +189,14 @@ const useGroupInfo = () => {
       );
       await channelChat.create();
       await channelChat.addMembers([selectedUser.user_id]);
-      await channelChat.sendMessage({
-        text: `${profile.myProfile.username} remove you from ${channel.data.name}`,
-        isRemoveMember: true,
-        silent: true
-      });
+      await channelChat.sendMessage(
+        {
+          text: `${profile.myProfile.username} remove you from ${channel.data.name}`,
+          isRemoveMember: true,
+          silent: true
+        },
+        {skip_push: true}
+      );
       // await channel.sendMessage(
       //   {
       //     text: `${profile.myProfile.username} removed ${selectedUser.user.name} from this chat`,
