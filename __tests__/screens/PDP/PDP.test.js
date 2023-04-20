be365a4cca5d3100d6fd30826dcf791a5d6aabdf
agitafirstawan26@gmail.com <agitafirstawan26@gmail.com>
2023-04-21 01:58:53 +0700

    fix: fix connector style

--- __tests__/screens/PDP/PDP.test.js
+++ __tests__/screens/PDP/PDP.test.js
@@ -1,49 +1,49 @@
-import React from 'react'
-import {render, cleanup} from '@testing-library/react-native'
-import PDP from '../../../src/screens/PostPageDetail'
-import Store from '../../../src/context/Store'
-
+import React from 'react';
+import {render, cleanup} from '@testing-library/react-native';
+import PDP from '../../../src/screens/PostPageDetail';
+import Store from '../../../src/context/Store';
 
 jest.mock('react-native-activity-feed/node_modules/react-native-image-crop-picker', () => ({
-    openPicker: jest.fn()
-}))
+  openPicker: jest.fn()
+}));
 
 jest.mock('../../../src/hooks/useAfterInteractions', () => ({
-    useAfterInteractions: () => ({
-        transitionRef: {current: {animateNextTransition: jest.fn()}}, interactionsComplete: true
-    })
-}))
-const mockedGoBack = jest.fn()
+  useAfterInteractions: () => ({
+    transitionRef: {current: {animateNextTransition: jest.fn()}},
+    interactionsComplete: true
+  })
+}));
+const mockedGoBack = jest.fn();
 jest.mock('@react-navigation/native', () => ({
-    ...jest.requireActual('@react-navigation/native'),
-    useRoute: () => ({
-        params: {
-            isCaching: jest.fn().mockImplementation(true)
-        }
-    }),
-    useNavigation: () => ({
-        goBack: mockedGoBack
-    }),
-}))
+  ...jest.requireActual('@react-navigation/native'),
+  useRoute: () => ({
+    params: {
+      isCaching: jest.fn().mockImplementation(true)
+    }
+  }),
+  useNavigation: () => ({
+    goBack: mockedGoBack
+  })
+}));
 
 describe('PDP page should run correctly', () => {
-    const props = {
-        route: {
-            params: {
-                isCaching: false,
-                refreshCache: jest.fn()
-            }
-        }
+  const props = {
+    route: {
+      params: {
+        isCaching: false,
+        refreshCache: jest.fn()
+      }
     }
+  };
 
-    afterEach(cleanup)
+  afterEach(cleanup);
 
-    it('should be same as snapshot', () => {
-        const {toJSON} = render(<PDP {...props} />, {wrapper: Store})
-        expect(toJSON).toMatchSnapshot()
-    })
+  it('should be same as snapshot', () => {
+    const {toJSON} = render(<PDP {...props} />, {wrapper: Store});
+    expect(toJSON).toMatchSnapshot();
+  });
 
-    it('refresh cache should call if refreshCache params exist', () => {
-        expect(props.route.params.refreshCache).toHaveBeenCalled()
-    })
-})
+  it('refresh cache should call if refreshCache params exist', () => {
+    expect(props.route.params.refreshCache).toHaveBeenCalled();
+  });
+});
