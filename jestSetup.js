91d866bbeda5471bbe8e455e65b0901334d6a05b
agitafirstawan26@gmail.com <agitafirstawan26@gmail.com>
2023-04-26 17:58:13 +0700

    fix: rolllback enable husky

--- jestSetup.js
+++ jestSetup.js
@@ -1,29 +1,30 @@
 import Adapter from 'enzyme-adapter-react-16';
-import { configure } from 'enzyme';
+import {configure} from 'enzyme';
 
 configure({
-    adapter: new Adapter(),
+  adapter: new Adapter()
 });
 
 // Auto-mock YellowBox component.
 // jest.mock('YellowBox');
 
 const mockConsoleMethod = (realConsoleMethod) => {
-    const ignoredMessages = ['test was not wrapped in act(...)'];
+  const ignoredMessages = ['test was not wrapped in act(...)'];
 
-    return (message, ...args) => {
-        const containsIgnoredMessage = ignoredMessages.some((ignoredMessage) =>
-            message.includes(ignoredMessage),
-        );
+  return (message, ...args) => {
+    const containsIgnoredMessage = ignoredMessages.some((ignoredMessage) =>
+      message.includes(ignoredMessage)
+    );
 
-        if (!containsIgnoredMessage) {
-            realConsoleMethod(message, ...args);
-        }
-    };
+    if (!containsIgnoredMessage) {
+      realConsoleMethod(message, ...args);
+    }
+  };
 };
 
 // Suppress console errors and warnings to avoid polluting output in tests.
 jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
 // jest.mock('NativeAnimatedHelp');
+global.__reanimatedWorkletInit = jest.fn();
 console.warn = jest.fn(mockConsoleMethod(console.warn));
 console.error = jest.fn(mockConsoleMethod(console.error));
