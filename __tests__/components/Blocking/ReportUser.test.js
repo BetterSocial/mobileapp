11b5be0f023bd0c63df4db7ae0e07158b6b44fa3
agitafirstawan26@gmail.com <agitafirstawan26@gmail.com>
2023-04-27 20:06:10 +0700

    fix: fix issue push notif

---
+++ __tests__/components/Blocking/ReportUser.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReportUser from '../../../src/components/Blocking/ReportPostAnonymous';
+import {render, fireEvent} from '@testing-library/react-native';
+
+describe('ReportUSer should run correctly', () => {
+  it('should match snapshot', () => {
+    const ref = jest.fn();
+    const onSelect = jest.fn();
+    const onSkip = jest.fn();
+    const {toJSON} = render(
+      <ReportUser refReportPostAnonymous={ref} onSelect={onSelect} onSkip={onSkip} />
+    );
+    expect(toJSON).toMatchSnapshot();
+  });
+});
