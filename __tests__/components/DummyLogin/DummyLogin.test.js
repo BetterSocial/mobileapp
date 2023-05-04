cec713c56bd57c8aacbdf86396430783ae31c43f
Muhammad Fajariansyah Ismail <fajarism@RokaM1.local>
2023-05-04 20:24:54 +0700

    test: unit test and snapshot for dev dummy login

--- __tests__/components/DummyLogin/DummyLogin.test.js
+++ __tests__/components/DummyLogin/DummyLogin.test.js
@@ -1,39 +1,60 @@
-import React from 'react'
-import { render, cleanup, fireEvent } from '@testing-library/react-native';
-import DummyLogin from '../../../src/components/DevDummyLogin'
-import Store from '../../../src/context/Store';
-import * as actionUser from '../../../src/context/actions/users';
+import React from 'react';
+import renderer from 'react-test-renderer';
+import {cleanup, fireEvent, render} from '@testing-library/react-native';
 
-const mockNavigate = jest.fn()
+import * as actionUser from '../../../src/context/actions/users';
+import DummyLogin from '../../../src/components/DevDummyLogin';
+import Store, {Context} from '../../../src/context/Store';
+import {usersState} from '../../../src/context/reducers/userReducer';
+
+const mockNavigate = jest.fn();
 jest.mock('@react-navigation/native', () => ({
   ...jest.requireActual('@react-navigation/native'),
-  useNavigation: () => ({ goBack: jest.fn(), navigate: mockNavigate}),
+  useNavigation: () => ({goBack: jest.fn(), navigate: mockNavigate}),
   useRoute: () => ({
     params: {}
-  }),
+  })
+}));
+
+jest.mock('../../../src/utils/getstream/ClientGetStram', () => ({
+  useClientGetstream: () => ({
+    create: jest.fn()
+  })
+}));
+
+jest.mock('react-native-config', () => ({
+  ENABLE_DEV_ONLY_FEATURE: 'true'
 }));
 
 describe('DummyLogin should run correctly', () => {
-    afterEach(cleanup)
-    it('should match snapshot', () => {    
-        const reset = jest.fn() 
-        const {toJSON} = render(<DummyLogin resetClickTime={reset} />, {wrapper: Store})
-        expect(toJSON).toMatchSnapshot()
-    })
+  afterEach(cleanup);
+  it('should match snapshot', () => {
+    const contextValue = {
+      users: [usersState, () => jest.fn()]
+    };
 
-    it('closeDummy login should run correctly', () => {
-        const reset = jest.fn() 
-        const {getByTestId} = render(<DummyLogin resetClickTime={reset} />, {wrapper: Store})
-        fireEvent.press(getByTestId('closedemo'))
-        expect(reset).toHaveBeenCalled()
-    })
+    const reset = jest.fn();
+    const {toJSON} = renderer.create(
+      <Context.Provider value={contextValue}>
+        <DummyLogin resetClickTime={reset} />;
+      </Context.Provider>
+    );
+    expect(toJSON()).toMatchSnapshot();
+  });
 
-    it('dummyOnBoarding should run correctly', () => {
-        const reset = jest.fn() 
-        const spyHumanId = jest.spyOn(actionUser, 'setDataHumenId')
-        const {getByTestId} = render(<DummyLogin resetClickTime={reset} />, {wrapper: Store})
-        fireEvent.press(getByTestId('dummyonboarding'))
-        expect(spyHumanId).toHaveBeenCalled()
-        expect(mockNavigate).toHaveBeenCalled()
-    })
-})
+  it('closeDummy login should run correctly', () => {
+    const reset = jest.fn();
+    const {getByTestId} = render(<DummyLogin resetClickTime={reset} />, {wrapper: Store});
+    fireEvent.press(getByTestId('closedemo'));
+    expect(reset).toHaveBeenCalled();
+  });
+
+  it('dummyOnBoarding should run correctly', () => {
+    const reset = jest.fn();
+    const spyHumanId = jest.spyOn(actionUser, 'setDataHumenId');
+    const {getByTestId} = render(<DummyLogin resetClickTime={reset} />, {wrapper: Store});
+    fireEvent.press(getByTestId('dummyonboarding'));
+    expect(spyHumanId).toHaveBeenCalled();
+    expect(mockNavigate).toHaveBeenCalled();
+  });
+});
