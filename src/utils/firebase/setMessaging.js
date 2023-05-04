d8709f163ed91484681273e14c2fc0a265bc8938
Muhammad Fajariansyah Ismail <fajarism@RokaM1.local>
2023-05-04 12:33:40 +0700

    test: add firebase messaging unit test

--- src/utils/firebase/setMessaging.js
+++ src/utils/firebase/setMessaging.js
@@ -2,7 +2,7 @@ import messaging from '@react-native-firebase/messaging';
 
 export const setMessage = async (client) => {
   const token = await messaging().getToken();
-  const sendToken = await client.addDevice(token, 'firebase')
+  await client.addDevice(token, 'firebase');
   messaging().onTokenRefresh(async (newToken) => {
     await client.addDevice(newToken, 'firebase');
   });
