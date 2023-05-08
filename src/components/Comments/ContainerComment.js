92916eae7e5a7e608393b2074aba2f73787055a5
agitafirstawan26@gmail.com <agitafirstawan26@gmail.com>
2023-05-09 03:02:37 +0700

    fix: possible remove anon author comment

--- src/components/Comments/ContainerComment.js
+++ src/components/Comments/ContainerComment.js
@@ -36,7 +36,6 @@ const ContainerComment = ({
   const {calculationText, calculatedSizeScreen, calculatePaddingBtm} = usePostDetail();
   const {deleteCommentFromContext} = usePostContextHook(contextSource);
   const onCommentLongPressed = async (item, level = 0) => {
-    console.log('jalan2', item)
     const selfId = await getUserId();
     if (selfId === item?.user_id || item?.is_you) {
       setSelectedCommentForDelete(item);
