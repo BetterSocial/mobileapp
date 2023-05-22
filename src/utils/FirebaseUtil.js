374e23af4e25d68ae2bfb32feba40ae4e1f65de7
Muhammad Fajariansyah Ismail <fajarism@RokaM1.local>
2023-05-22 15:19:31 +0700

    fix: sonarcloud

--- src/utils/FirebaseUtil.js
+++ src/utils/FirebaseUtil.js
@@ -15,23 +15,12 @@ export const fetchRemoteConfig = () => {
       return remoteConfig().getAll();
     })
     .then((res) => {
-      //   return {
-      //     FORCE_UPDATE_APP_STORE_URL: snapshot.FORCE_UPDATE_APP_STORE_URL.asString(),
-      //     IS_APPLICATION_ON: !!snapshot.IS_APPLICATION_ON.asBoolean(),
-      //     IS_ANNOUNCEMENTS_ON: !!snapshot.IS_ANNOUNCEMENTS_ON.asBoolean(),
-      //   };
-      //   let data = snapshot.enable_chat.asBoolean();
-      //   console.log(data);
-      Object.entries(res).forEach(() => {
-        // const [key, entry] = $;
-      });
       return res;
     })
     .catch((err) => {
       if (__DEV__) {
         console.log('remote Configs error: ', err);
       }
-      // firebaseRemoteConfig;
     });
 };
 
