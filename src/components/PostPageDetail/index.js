4aba61a8696bf1c5293ec7236642e021f1eccadf
agitafirstawan26@gmail.com <agitafirstawan26@gmail.com>
2023-05-25 05:56:23 +0700

    fix: fix crash

--- src/components/PostPageDetail/index.js
+++ src/components/PostPageDetail/index.js
@@ -125,7 +125,7 @@ const PostPageDetailIdComponent = (props) => {
   };
 
   const onBottomPage = () => {
-    if (scrollViewRef && scrollViewRef.current && !loadingComment) {
+    if (scrollViewRef && scrollViewRef.current) {
       scrollViewRef.current.scrollToEnd({animated: true});
     }
   };
