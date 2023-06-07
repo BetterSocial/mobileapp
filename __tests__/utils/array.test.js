1a2aed62e430af325b624c4f60a89ecdbe23ef68
agitafirstawan26@gmail.com <agitafirstawan26@gmail.com>
2023-06-07 08:39:30 +0700

    fix: fix code smell

--- __tests__/utils/array.test.js
+++ __tests__/utils/array.test.js
@@ -1,4 +1,3 @@
-import {act} from 'react-test-renderer';
 import {ChunkArray, joinTopicIntoTopicList} from '../../src/utils/array/ChunkArray';
 
 describe('chunk array should run correctly', () => {
@@ -12,16 +11,3 @@ describe('chunk array should run correctly', () => {
     expect(joinTopicIntoTopicList('baru', ['baru'])).toEqual(['baru']);
   });
 });
-
-// export const ChunkArray = (arr, size) => {
-//   const myArray = [];
-//   for (let i = 0; i < arr.length; i += size) {
-//     myArray.push(arr.slice(i, i + size));
-//   }
-//   return myArray;
-// };
-
-// export const joinTopicIntoTopicList = (newTopic = '', topics = []) => {
-//   if (!topics?.includes(newTopic)) return [...topics, newTopic];
-//   return topics;
-// };
