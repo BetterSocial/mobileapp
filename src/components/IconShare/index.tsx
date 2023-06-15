0950974842458f6c9d653e9b3f925281c91f472b
agitafirstawan26@gmail.com <agitafirstawan26@gmail.com>
2023-06-15 11:41:27 +0700

    fix: init task for share icon

---
+++ src/components/IconShare/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Platform, Image } from 'react-native'
+
+/**
+ * @typedef {Object} IconShareProps
+ * @property {Function} onPress
+ */
+
+/**
+ *
+ * @param {IconShareProps} props
+ */
+
+
+const IconShare = (props) => {
+    const isIos = Platform.OS === 'ios'
+    if(isIos) {
+        return (
+            
+        )
+    }
+}
+
+
+export default IconShare
