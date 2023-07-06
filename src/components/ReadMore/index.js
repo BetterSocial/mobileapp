a13d0e0d11608f548e127fc1cd6242a8ce06f7b4
agita firstawan <agita.firstawan@selleri.id>
2023-07-06 08:19:53 +0700

    test: adding unit test

--- src/components/ReadMore/index.js
+++ src/components/ReadMore/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 import {colors} from '../../utils/colors';
+import useReadmore from './hooks/useReadmore';
 
 const styles = StyleSheet.create({
   moreText: {
@@ -22,64 +23,38 @@ const styles = StyleSheet.create({
  */
 
 const ReadMore = (props) => {
-  const [curNumberLine, setNumberLine] = React.useState(0);
-  const [charLength, setCharLength] = React.useState(0);
-  const [isFinishSetLayout, setIsFinishSetLayout] = React.useState(false);
-  const [lengthTextFirstLine, setLengthTextFirstLine] = React.useState(0);
-  const [realNumberLine, setRealNumberLine] = React.useState(0);
-  const [textShown, setTextShown] = React.useState('');
-  const [layoutWidth, setLayoutWidth] = React.useState(0);
-  const handleLayoutText = async ({nativeEvent}) => {
-    let characterNumber = 0;
-    let textWidth = 0;
-    let lengthFirstLine = 0;
-    let text = '';
-    // eslint-disable-next-line no-plusplus
-    for (let i = 0; i < props.numberLine; i++) {
-      characterNumber += nativeEvent.lines[i].text.length;
-      textWidth += nativeEvent.lines[i].width;
-      if (i === 0) {
-        lengthFirstLine = nativeEvent.lines[i].text.length;
-      }
-      if (i === props.numberLine - 1) {
-        let newText = `${nativeEvent.lines[i].text}`.replace(/\n/g, '');
-        if (nativeEvent.lines[i].width >= layoutWidth) {
-          newText = newText.substring(15);
-        }
-        text += newText;
-      } else {
-        text += nativeEvent.lines[i].text;
-      }
-    }
-    setTextShown(text);
-    setRealNumberLine(nativeEvent.lines.length);
-    setNumberLine(props.numberLine);
-    setCharLength(characterNumber);
-    setLengthTextFirstLine(lengthFirstLine);
-    setIsFinishSetLayout(true);
-  };
-
-  const handleLayoutWidth = ({nativeEvent}) => {
-    setLayoutWidth(Math.floor(nativeEvent.layout.width));
-  };
+  const {
+    isFinishSetLayout,
+    realNumberLine,
+    textShown,
+    layoutWidth,
+    setIsFinishSetLayout,
+    handleLayoutText,
+    handleLayoutWidth,
+    limitNumberLine
+  } = useReadmore({
+    numberLine: props.numberLine
+  });
 
   React.useEffect(() => {
     setIsFinishSetLayout(false);
-  }, [layoutWidth]);
+  }, [layoutWidth, props.text]);
   return (
     <View onLayout={handleLayoutWidth} style={props.containerStyle}>
       {isFinishSetLayout ? (
-        <TouchableOpacity onPress={props.onPress}>
+        <TouchableOpacity testID="finishLayout" onPress={props.onPress}>
           <Text>
             {textShown}{' '}
-            {props.numberLine < realNumberLine ? (
-              <Text style={styles.moreText}>More...</Text>
+            {limitNumberLine < realNumberLine ? (
+              <Text testID="moreText" style={styles.moreText}>
+                More...
+              </Text>
             ) : null}{' '}
           </Text>
         </TouchableOpacity>
       ) : null}
       {!isFinishSetLayout ? (
-        <Text numberOfLines={props.numberLine} onTextLayout={handleLayoutText}>
+        <Text testID="notFinishLayout" onTextLayout={handleLayoutText}>
           {props.text}{' '}
         </Text>
       ) : null}
