88a6e26f6112b934233da9dfa3cd5e0b1887d533
Wahyu Irwansyah <wahyuirwansyah@Wahyus-MacBook-Pro.local>
2023-07-24 22:12:22 +0700

    feat: add option anon dm (pull staging)

--- src/screens/GroupInfo/hooks/useGroupInfo.js
+++ src/screens/GroupInfo/hooks/useGroupInfo.js
@@ -32,6 +32,7 @@ const useGroupInfo = () => {
   const [selectedUser, setSelectedUser] = React.useState(null);
   const [newParticipant, setNewParticipan] = React.useState([]);
   const [openModal, setOpenModal] = React.useState(false);
+  const [showPopover, setShowPopover] = React.useState(false);
   const [, dispatchChannel] = React.useContext(Context).channel;
   const serializeMembersList = (result = []) => {
     if (!result) {
@@ -276,6 +277,7 @@ const useGroupInfo = () => {
   };
 
   const alertRemoveUser = async (status) => {
+    setShowPopover(false);
     if (status === 'view') {
       setOpenModal(false);
       handleOpenProfile(selectedUser).catch((e) => console.log(e));
@@ -292,6 +294,7 @@ const useGroupInfo = () => {
       await checkUserIsBlockHandle();
     }
   };
+
   const onLeaveGroup = () => {
     Alert.alert('', 'Exit this group?', [{text: 'Cancel'}, {text: 'Exit', onPress: leaveGroup}]);
   };
@@ -333,6 +336,7 @@ const useGroupInfo = () => {
 
   // eslint-disable-next-line consistent-return
   const handlePressContact = async (item) => {
+    setShowPopover(false);
     if (channelState?.channel.data.type === 'group') {
       await handleSelectUser(item);
       return true;
@@ -355,6 +359,15 @@ const useGroupInfo = () => {
         }
       });
     }, 500);
+  };
+
+  const handleOpenPopOver = async (user) => {
+    if (user.user_id === profile.myProfile.user_id) {
+      setShowPopover(false);
+      return;
+    }
+    await handleSelectUser(user);
+    setShowPopover(true);
   };
 
   return {
@@ -399,9 +412,10 @@ const useGroupInfo = () => {
     onReportGroup,
     setUsername,
     setSelectedUser,
-    openChatMessage,
     generateSystemChat,
-    setNewParticipan
+    showPopover,
+    setShowPopover,
+    handleOpenPopOver
   };
 };
 
