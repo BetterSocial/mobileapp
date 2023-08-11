8f4d1a2feb6c49aa8291149bb0e47c9a8728e196
agitafirstawan26@gmail.com <agitafirstawan26@gmail.com>
2023-08-12 06:23:50 +0700

    fix: fix code smell

--- src/components/Card/CardInLinkPreview.js
+++ src/components/Card/CardInLinkPreview.js
@@ -1,7 +1,14 @@
 import * as React from 'react';
 import _ from 'lodash';
 import PropTypes from 'prop-types';
-import {Linking, Text, TouchableNativeFeedback, TouchableOpacity, View} from 'react-native';
+import {
+  Linking,
+  Text,
+  TouchableNativeFeedback,
+  TouchableOpacity,
+  View,
+  StyleSheet
+} from 'react-native';
 
 import Gap from '../Gap';
 import Header from './CardHeaderInLinkPreview';
@@ -9,7 +16,87 @@ import NewsEmptyState from '../../assets/images/news-empty-state.png';
 import TestIdConstant from '../../utils/testId';
 import Image from '../Image';
 import {sanitizeUrlForLinking} from '../../utils/Utils';
-import CardStyle from './style/styles';
+import {COLORS} from '../../utils/theme';
+import {fonts} from '../../utils/fonts';
+
+const CardStyle = StyleSheet.create({
+  link: {
+    color: COLORS.blue,
+    textDecorationLine: 'underline',
+    marginStart: 8,
+    fontSize: 12
+  },
+  contentDomain: {flexDirection: 'row', alignItems: 'center'},
+  containerDomain: {justifyContent: 'space-around'},
+  date: {fontSize: 12, color: '#828282'},
+  domain: {
+    fontSize: 16,
+    lineHeight: 16,
+    color: '#000000',
+    fontWeight: 'bold',
+    fontFamily: fonts.inter[600]
+  },
+  width: (width) => ({
+    width
+  }),
+  imageHeader: {height: '100%', width: '100%', borderRadius: 45},
+  constainerHeader: {
+    flexDirection: 'row',
+    padding: 8
+  },
+  contentHeader: {
+    borderRadius: 45,
+    borderWidth: 0.2,
+    borderColor: 'rgba(0,0,0,0.5)',
+    width: 36,
+    height: 36,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  container: {
+    borderWidth: 0.5,
+    borderRadius: 8,
+    borderColor: 'rgba(0,0,0, 0.5)',
+    overflow: 'hidden',
+    paddingBottom: 8,
+    flex: 1
+  },
+  image: {
+    width: '100%',
+    height: '100%'
+  },
+  content: {
+    // paddingTop: 8,
+    justifyContent: 'space-between',
+    flex: 1
+    // backgroundColor: 'blue',
+  },
+  title: {
+    color: '#000000',
+    fontWeight: '600',
+    fontSize: 14,
+    lineHeight: 17,
+    marginBottom: 8,
+    fontFamily: fonts.inter[600],
+    paddingRight: 20,
+    paddingLeft: 20
+  },
+  description: {
+    color: COLORS.blackgrey,
+    fontSize: 12,
+    fontFamily: fonts.inter[400],
+    paddingRight: 20,
+    paddingLeft: 20,
+    marginTop: 5,
+    lineHeight: 18
+  },
+  openLinkText: {
+    color: COLORS.blue,
+    textDecorationLine: 'underline',
+    marginStart: 8,
+    fontSize: 12
+  }
+});
 
 const Card = (props) => {
   const {
