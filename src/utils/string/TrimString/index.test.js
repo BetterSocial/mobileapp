fac3f0a9a80cf6dd33a4b4b15dbfc0b431538d0a
Wahyu Irwansyah <wahyuiwangsyah@gmail.com>
2023-08-16 19:44:42 +0700

    feat: 2270 - align designs between 4 screens (merge staging)

--- src/utils/string/TrimString/index.test.js
+++ src/utils/string/TrimString/index.test.js
@@ -3,8 +3,3 @@ import {addDotAndRemoveNewline, trimString} from './index';
 it('testing trim String', () => {
   expect(trimString('saya suka makan sayur', 10)).toEqual('saya suka ... ');
 });
-
-// it('should remove white space and add dot', () => {
-//   expect(addDotAndRemoveNewline('hello  ')).toEqual('hello.');
-//   expect(addDotAndRemoveNewline('hello    ')).toEqual('hello.');
-// });
