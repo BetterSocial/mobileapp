27f889885f930f6c82d484b9af6e9c384cde1c6c
Muhammad Fajariansyah Ismail <fajarism@RokaM1.local>
2023-08-19 11:00:38 +0700

    fix: remove access token

--- src/utils/getstream/streamer.js
+++ src/utils/getstream/streamer.js
@@ -1,16 +1,16 @@
-import config from 'react-native-config'
-import { connect } from 'getstream';
+import config from 'react-native-config';
+import {connect} from 'getstream';
 
 const clientStream = (userToken) => {
   try {
-    const client = connect(config.STREAM_API_KEY, userToken.id, config.STREAM_APP_ID);
+    const client = connect(config.STREAM_API_KEY, userToken, config.STREAM_APP_ID);
     return client;
   } catch (e) {
     if (__DEV__) {
       console.log('error client stream connect: ', e);
     }
+    return null;
   }
-
 };
 
 export default clientStream;
