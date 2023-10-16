a641dfddb86c723302a0aed8d9e09e6174bab3f1
agitafirstawan26@gmail.com <agitafirstawan26@gmail.com>
2023-10-16 15:49:14 +0700

    test: remove duplicate lines

--- __tests__/screens/GroupInfo/GroupInfo.test.js
+++ __tests__/screens/GroupInfo/GroupInfo.test.js
@@ -5,6 +5,7 @@ import {act, fireEvent, render} from '@testing-library/react-native';
 import * as useGroupInfo from '../../../src/screens/GroupInfo/hooks/useGroupInfo';
 import GroupInfo, {styles} from '../../../src/screens/GroupInfo';
 import {Context} from '../../../src/context/Store';
+import {myProfileMock} from '../../../__mocks__/mockMyProfile';
 
 jest.mock('react-native-permissions', () => require('react-native-permissions/mock'));
 
@@ -32,40 +33,7 @@ jest.mock('stream-chat-react-native-core', () => ({
 }));
 
 describe('GroupInfo should run correctly', () => {
-  const mockMyProfile = {
-    bio: 'Fe mobile engineer',
-    country_code: 'ID',
-    createdAt: '2022-06-10T13:11:53.000Z',
-    follower: 13,
-    follower_symbol: '< 25',
-    following: 10,
-    following_symbol: '< 25',
-    human_id: 'I4K3M10FGR78EWQQDNQ2',
-    last_active_at: '2022-06-10T13:11:53.000Z',
-    location: [
-      {
-        city: 'Yauco, PR',
-        country: 'US',
-        createdAt: '2022-05-30T14:15:24.000Z',
-        location_id: '45',
-        location_level: 'City',
-        neighborhood: '',
-        slug_name: '',
-        state: 'Puerto Rico',
-        status: 'Y',
-        updatedAt: '2022-05-30T14:15:24.000Z'
-      }
-    ],
-    profile_pic_asset_id: '6f47f70bea98469f4a24b6ffc550c983',
-    profile_pic_path:
-      'https://res.cloudinary.com/hpjivutj2/image/upload/v1659099243/pbdv3jlyd4mhmtis6kqx.jpg',
-    profile_pic_public_id: 'pbdv3jlyd4mhmtis6kqx',
-    real_name: null,
-    status: 'Y',
-    updatedAt: '2022-07-29T12:54:04.000Z',
-    user_id: 'c6c91b04-795c-404e-b012-ea28813a2006',
-    username: 'Agita'
-  };
+  const mockMyProfile = myProfileMock;
 
   const mockAsset = [
     {
