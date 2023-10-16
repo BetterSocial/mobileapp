d898f8159f8b82023706d4695e7bdcbd101c82a5
Muhammad Fajariansyah Ismail <fajarism@RokaM1.local>
2023-10-16 23:26:52 +0700

    fix: hotfix react native image picker

--- src/screens/CreateGroupScreen/index.js
+++ src/screens/CreateGroupScreen/index.js
@@ -1,38 +1,37 @@
 import * as React from 'react';
-import { View, Text, FlatList, StatusBar } from 'react-native';
 import Toast from 'react-native-simple-toast';
-import { launchImageLibrary } from 'react-native-image-picker';
+import {Alert, FlatList, StatusBar, Text, View} from 'react-native';
+import {launchImageLibrary} from 'react-native-image-picker';
 
-import { createChannel } from '../../service/chat';
-
-import { Avatar, Gap } from '../../components';
-import { COLORS, FONTS, SIZES } from '../../utils/theme';
 import GroupName from './elements/GroupName';
-import StringConstant from '../../utils/string/StringConstant';
 import Header from './elements/Header';
-import { getUserId } from '../../utils/users';
-import { requestExternalStoragePermission } from '../../utils/permission';
-import { Analytics } from '../../libraries/analytics/firebaseAnalytics';
+import StringConstant from '../../utils/string/StringConstant';
+import {Analytics} from '../../libraries/analytics/firebaseAnalytics';
+import {Avatar, Gap} from '../../components';
+import {COLORS, FONTS, SIZES} from '../../utils/theme';
+import {createChannel} from '../../service/chat';
+import {getUserId} from '../../utils/users';
+import {requestExternalStoragePermission} from '../../utils/permission';
 
 const DUMMY = [
   {
     id: '88353551-b9bd-4cf5-a89e-ce6197b880c0',
     name: 'fajarism',
-    icon: 'https://res.cloudinary.com/hpjivutj2/image/upload/v1625213019/i3u9uptxnylfmfqevabf.jpg',
+    icon: 'https://res.cloudinary.com/hpjivutj2/image/upload/v1625213019/i3u9uptxnylfmfqevabf.jpg'
   },
   {
     id: 'c8a7d99d-51b5-4bb4-b9c8-cf936156f887',
     name: 'bas_v1-4',
-    icon: 'http://res.cloudinary.com/hpjivutj2/image/upload/v1623291797/t4girkwj1qfqxhr72rgn.jpg',
+    icon: 'http://res.cloudinary.com/hpjivutj2/image/upload/v1623291797/t4girkwj1qfqxhr72rgn.jpg'
   },
   {
     id: 'c8c39e52-5484-465c-b635-3c46384b6f2e',
     name: 'eka',
-    icon: 'https://res.cloudinary.com/hpjivutj2/image/upload/v1617245336/Frame_66_1_xgvszh.png',
-  },
+    icon: 'https://res.cloudinary.com/hpjivutj2/image/upload/v1617245336/Frame_66_1_xgvszh.png'
+  }
 ];
 
-const CreateGroupScreen = ({ navigation }) => {
+const CreateGroupScreen = ({navigation}) => {
   const [groupName, setGroupName] = React.useState(null);
   const [groupIcon, setGroupIcon] = React.useState(null);
   const [userId, setUserId] = React.useState(null);
@@ -47,21 +46,21 @@ const CreateGroupScreen = ({ navigation }) => {
 
   const handleImageLibrary = async () => {
     Analytics.logEvent('btn_take_photo_profile', {
-      id: 2,
+      id: 2
     });
-    const { success, message } = await requestExternalStoragePermission();
+    const {success, message} = await requestExternalStoragePermission();
     if (success) {
-      launchImageLibrary({ mediaType: 'photo' }, (res) => {
-        const image = {
-          uri: res.uri,
-          type: res.type, // or photo.type
-          name: res.fileName,
-        };
-        setGroupIcon(image);
-        // if (res.base64) {
-        //   // setImage(`${res.base64}`, dispatch);
-        //   setGroupIcon(`data:image/png;base64,${res.base64}`);
-        // }
+      launchImageLibrary({mediaType: 'photo'}, (res) => {
+        const assets = res.assets || [];
+        if (assets.length > 0) {
+          const {uri, type, fileName} = assets[0];
+          const image = {
+            uri,
+            type,
+            name: fileName
+          };
+          setGroupIcon(image);
+        }
       });
     } else {
       Toast.show(message, Toast.SHORT);
@@ -73,7 +72,7 @@ const CreateGroupScreen = ({ navigation }) => {
       const members = DUMMY.map((item) => item.id);
       members.push(userId);
       await createChannel('messaging', members, groupName);
-      alert('success create group');
+      Alert.alert('success create group');
     } catch (error) {
       if (__DEV__) {
         console.log('create new channel error: ', error);
@@ -81,29 +80,26 @@ const CreateGroupScreen = ({ navigation }) => {
     }
   };
 
-  const renderItem = ({ item, index }) => (
-    <View
-      key={index}
-      style={{ flexDirection: 'row', marginBottom: SIZES.base * 2 }}>
+  const renderItem = ({item, index}) => (
+    <View key={index} style={{flexDirection: 'row', marginBottom: SIZES.base * 2}}>
       <Avatar image={item.icon} />
       <Gap width={SIZES.base} />
-      <View style={{ justifyContent: 'center' }}>
+      <View style={{justifyContent: 'center'}}>
         <Text>{item.name}</Text>
       </View>
     </View>
   );
 
   return (
-    <View style={{ flex: 1 }}>
+    <View style={{flex: 1}}>
       <StatusBar translucent={false} />
       <Header
         title={StringConstant.chatTabHeaderCreateChatButtonText}
-        containerStyle={{ marginHorizontal: 16 }}
+        containerStyle={{marginHorizontal: 16}}
         subTitle={groupName ? 'Finish' : 'Skip'}
-        subtitleStyle={{ color: COLORS.holyTosca }}
+        subtitleStyle={{color: COLORS.holyTosca}}
         onPressSub={() => createNewChannel()}
         onPress={() => navigation.goBack()}
-
       />
       <GroupName
         onPress={() => handleImageLibrary()}
@@ -111,15 +107,12 @@ const CreateGroupScreen = ({ navigation }) => {
         groupIcon={groupIcon}
       />
 
-      <View style={{ marginHorizontal: 16, marginTop: SIZES.base }}>
-        <Text style={{ ...FONTS.h2, color: COLORS.holyTosca }}>Participants</Text>
+      <View style={{marginHorizontal: 16, marginTop: SIZES.base}}>
+        <Text style={{...FONTS.h2, color: COLORS.holyTosca}}>Participants</Text>
       </View>
 
-      <View style={{ marginHorizontal: 16, marginTop: SIZES.base }}>
-        <FlatList
-          data={DUMMY}
-          renderItem={renderItem}
-        />
+      <View style={{marginHorizontal: 16, marginTop: SIZES.base}}>
+        <FlatList data={DUMMY} renderItem={renderItem} />
       </View>
     </View>
   );
--- src/screens/CreatePost/elements/SheetMedia.js
+++ src/screens/CreatePost/elements/SheetMedia.js
@@ -1,10 +1,9 @@
 import * as React from 'react';
-import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
-
 import RBSheet from 'react-native-raw-bottom-sheet';
+import {ActivityIndicator, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 
-import MemoIc_camera from '../../../assets/icons/Ic_camera';
 import MemoIcCreatePoll from '../../../assets/icons/ic_create_poll';
+import MemoIc_camera from '../../../assets/icons/Ic_camera';
 import MemoIc_media from '../../../assets/icons/Ic_media';
 import {colors} from '../../../utils/colors';
 import {fonts} from '../../../utils/fonts';
@@ -15,40 +14,45 @@ const SheetMedia = ({
   takePhoto,
   createPoll,
   medias = [],
+  isLoadingUploadingMedia = false,
+  isLoadingUploadingPhoto = false
 }) => (
-    <RBSheet
-      ref={refMedia}
-      closeOnDragDown={true}
-      closeOnPressMask={true}
-      customStyles={{
-        container: styles.containerSheet,
-        draggableIcon: styles.draggableIcon,
-      }}>
-      <View style={styles.container}>
-        {medias.length === 0 && (
-          <List
-            label="Add a poll"
-            icon={<MemoIcCreatePoll width={16.67} height={16.67} />}
-            onPress={createPoll}
-          />
-        )}
+  <RBSheet
+    ref={refMedia}
+    closeOnDragDown={true}
+    closeOnPressMask={true}
+    customStyles={{
+      container: styles.containerSheet,
+      draggableIcon: styles.draggableIcon
+    }}>
+    <View style={styles.container}>
+      {medias.length === 0 && (
         <List
-          label="Upload media from library"
-          icon={<MemoIc_media width={16.67} height={16.67} />}
-          onPress={uploadFromMedia}
+          label="Add a poll"
+          icon={<MemoIcCreatePoll width={16.67} height={16.67} />}
+          onPress={createPoll}
         />
-        <List
-          label="Take a photo"
-          icon={<MemoIc_camera width={16.67} height={16.67} />}
-          onPress={takePhoto}
-        />
-      </View>
-    </RBSheet>
-  );
-const List = ({icon, label, onPress}) => (
+      )}
+      <List
+        label="Upload media from library"
+        icon={<MemoIc_media width={16.67} height={16.67} />}
+        onPress={uploadFromMedia}
+        isLoading={isLoadingUploadingMedia}
+      />
+      <List
+        label="Take a photo"
+        icon={<MemoIc_camera width={16.67} height={16.67} />}
+        onPress={takePhoto}
+        isLoading={isLoadingUploadingPhoto}
+      />
+    </View>
+  </RBSheet>
+);
+const List = ({icon, label, onPress, isLoading = false}) => (
   <TouchableOpacity style={styles.list} onPress={onPress}>
     {icon}
     <Text style={styles.labelList}>{label}</Text>
+    {isLoading ? <ActivityIndicator size="small" color="#0000ff" style={styles.loader} /> : <></>}
   </TouchableOpacity>
 );
 export default SheetMedia;
@@ -57,7 +61,7 @@ const styles = StyleSheet.create({
   container: {
     paddingHorizontal: 20,
     paddingTop: 20,
-    paddingBottom: 38,
+    paddingBottom: 38
   },
   list: {
     flexDirection: 'row',
@@ -66,19 +70,23 @@ const styles = StyleSheet.create({
     backgroundColor: colors.lightgrey,
     borderRadius: 8,
     marginBottom: 12,
-    alignItems: 'center',
+    alignItems: 'center'
   },
   labelList: {
     fontFamily: fonts.inter[500],
     fontSize: 14,
     color: colors.black,
     paddingLeft: 8,
+    flex: 1
   },
   containerSheet: {
     borderTopRightRadius: 20,
-    borderTopLeftRadius: 20,
+    borderTopLeftRadius: 20
   },
   draggableIcon: {
-    backgroundColor: colors.alto,
+    backgroundColor: colors.alto
   },
+  loader: {
+    marginRight: 10
+  }
 });
