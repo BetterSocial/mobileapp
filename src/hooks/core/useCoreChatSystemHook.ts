39f53fef8f906dd64517e56530e87d4ac68a7310
agitafirstawan26@gmail.com <agitafirstawan26@gmail.com>
2023-10-24 13:30:24 +0700

    feat: fix code smell

--- src/hooks/core/useCoreChatSystemHook.ts
+++ src/hooks/core/useCoreChatSystemHook.ts
@@ -66,13 +66,6 @@ const useCoreChatSystemHook = () => {
 
   const {lastJsonMessage, lastSignedMessage} = useBetterWebsocketHook();
 
-  const channelImage = (websocket: GetstreamWebsocket) => {
-    const roleMember = websocket?.channel?.members?.find(
-      (channel) => channel?.user?.name === websocket?.targetName
-    );
-    return roleMember?.user;
-  };
-
   const saveChannelListData = async (
     websocketData: GetstreamWebsocket,
     channelType: 'PM' | 'ANON_PM'
