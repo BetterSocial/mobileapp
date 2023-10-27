5fdfeba429e41a71ea374dc89956ec7a203fba58
Ari Lukman P <ari.lukman.p@gmail.com>
2023-10-27 09:46:35 +0700

    feat: 2740 update channel style

--- src/components/AnonymousChat/BaseChannelItemStyles.tsx
+++ src/components/AnonymousChat/BaseChannelItemStyles.tsx
@@ -1,21 +1,22 @@
 import {StyleSheet} from 'react-native';
 
+import dimen from '../../utils/dimen';
 import {colors} from '../../utils/colors';
 
 const BaseChatItemStyles = StyleSheet.create({
   chatContainer: {
     display: 'flex',
     flexDirection: 'row',
-    paddingTop: 14,
-    paddingLeft: 24,
+    paddingTop: dimen.normalizeDimen(14),
+    paddingLeft: dimen.normalizeDimen(20),
     backgroundColor: colors.white
   },
   chatContentContainer: {
     display: 'flex',
     flexDirection: 'column',
-    paddingStart: 15,
-    paddingEnd: 20,
-    paddingBottom: 14,
+    paddingStart: dimen.normalizeDimen(15),
+    paddingEnd: dimen.normalizeDimen(20),
+    paddingBottom: dimen.normalizeDimen(14),
     flex: 1,
     borderBottomColor: colors.alto,
     borderBottomWidth: 1
