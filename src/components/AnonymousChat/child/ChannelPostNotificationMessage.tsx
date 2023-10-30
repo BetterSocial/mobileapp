4dbdeca7c14569bef69d97516917ab1072988705
Ari Lukman P <ari.lukman.p@gmail.com>
2023-10-30 10:54:44 +0700

    feat: ping 2740 update channel list with dimen

--- src/components/AnonymousChat/child/ChannelPostNotificationMessage.tsx
+++ src/components/AnonymousChat/child/ChannelPostNotificationMessage.tsx
@@ -3,7 +3,7 @@ import {StyleSheet, Text} from 'react-native';
 
 import {BaseChannelItemTypeProps} from '../../../../types/component/AnonymousChat/BaseChannelItem.types';
 import {colors} from '../../../utils/colors';
-import {fonts} from '../../../utils/fonts';
+import {fonts, normalize} from '../../../utils/fonts';
 
 const ChannelPostNotificationMessage = ({
   type = BaseChannelItemTypeProps.ANON_PM,
@@ -13,14 +13,14 @@ const ChannelPostNotificationMessage = ({
   const styles = StyleSheet.create({
     chatContentPostNotificationMessage: {
       fontFamily: fonts.inter[400],
-      fontSize: 12,
+      fontSize: normalize(12),
       lineHeight: 20,
       color: colors.gray,
       marginRight: 29
     },
     chatContentPostNotificationMessageBold: {
       fontFamily: fonts.inter[600],
-      fontSize: 12,
+      fontSize: normalize(12),
       lineHeight: 20,
       color: colors.gray
     }
