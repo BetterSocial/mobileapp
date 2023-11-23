4179f25459827e433f571345c9433a28128f4aca
Febri Maulana Yunus <febriyunus@gmail.com>
2023-11-23 23:46:41 +0700

    feat: ping-2359 refactor code useMoveChatTypeHook

--- src/hooks/core/chat/useMoveChatTypeHook.ts
+++ src/hooks/core/chat/useMoveChatTypeHook.ts
@@ -22,11 +22,13 @@ interface MoveToChatChannelParams {
 const useMoveChatTypeHook = () => {
   const {localDb, refresh} = useLocalDatabaseHook();
   const {signedProfileId} = useUserAuthHook();
-
   const {goToMoveChat} = useChatUtilsHook();
 
-  const saveChannelList = async (channel: any, channelCategory: ChannelCategory) => {
-    if (channel?.members?.length === 0) return Promise.reject(Error('no members'));
+  const saveChannelList = async (
+    channel: any,
+    channelCategory: ChannelCategory
+  ): Promise<ChannelList | undefined> => {
+    if (!channel?.members?.length) return Promise.reject(Error('no members'));
 
     const isAnonymous = channelCategory === ANONYMOUS;
     const type: {[key: string]: ChannelType} = {
@@ -42,19 +44,20 @@ const useMoveChatTypeHook = () => {
       const myUserData = channel?.members?.find(
         (member) => member?.user?.id === signedProfileId
       )?.user;
-      const signedChannelUsername = myUserData?.username ?? myUserData?.name;
       signedChannelName =
         channel?.channel_type === 4
           ? `Anonymous ${channel?.anon_user_info_emoji_name}`
-          : getChatName(channel?.name, signedChannelUsername);
+          : getChatName(channel?.name, myUserData?.username ?? myUserData?.name);
 
-      if (channel?.type === 'group' || channel?.type === 'topics') {
-        signedChannelImage = channel?.channel_image;
-      } else {
-        signedChannelImage =
-          channel?.members?.find((member) => member?.user_id !== signedProfileId)?.user?.image ??
-          DEFAULT_PROFILE_PIC_PATH;
-      }
+      signedChannelImage =
+        channel?.type === 'group' || channel?.type === 'topics'
+          ? channel?.channel_image
+          : channel?.members?.find((member) => member?.user_id !== signedProfileId)?.user?.image ??
+            DEFAULT_PROFILE_PIC_PATH;
+
+      channel.myUserId = signedProfileId;
+    } else {
+      channel.myUserId = undefined;
     }
 
     const chatName = isAnonymous
@@ -63,28 +66,31 @@ const useMoveChatTypeHook = () => {
 
     channel.targetName = chatName?.name;
     channel.targetImage = chatName?.image;
-    if (isAnonymous) {
-      channel.firstMessage = channel?.messages?.[0];
-    } else {
-      channel.firstMessage = channel?.messages?.[channel?.messages?.length - 1];
-      channel.myUserId = signedProfileId;
-    }
+    channel.firstMessage = isAnonymous
+      ? channel?.messages?.[0]
+      : channel?.messages?.[channel?.messages?.length - 1];
     channel.channel = {...channel};
-    const channelType = channel?.type;
 
+    const channelType = channel?.type;
     const channelList = ChannelList.fromChannelAPI(channel, type[channelType], channel?.members);
+
     await channelList.saveIfLatest(localDb);
-    refresh('channelList');
-    refresh('chat');
-    refresh('channelInfo');
-    refresh('channelMember');
+    refresh('channelList', 'chat', 'channelInfo', 'channelMember');
     goToMoveChat(channelList);
+
     return channelList;
   };
 
-  const saveChannelData = async (channel: any, channelCategory: ChannelCategory) => {
-    if (!channel?.members || channel?.members?.length === 0) return console.info('no members');
-    if (channel?.type === 'topics') return console.info('type is topics');
+  const saveChannelData = async (channel: any, channelCategory: ChannelCategory): Promise<void> => {
+    if (!channel?.members?.length) {
+      console.info('no members');
+      return;
+    }
+
+    if (channel?.type === 'topics') {
+      console.info('type is topics');
+      return;
+    }
 
     try {
       await Promise.all(
@@ -108,56 +114,54 @@ const useMoveChatTypeHook = () => {
         })
       );
 
-      return await saveChannelList(channel, channelCategory);
+      await saveChannelList(channel, channelCategory);
     } catch (e) {
       console.log('error on saveChannelData', e);
-      return e;
     }
   };
 
-  const moveToSignedChannel = async ({targetUserId, oldChannelId}: MoveToChatChannelParams) => {
+  const moveToChannel = async (
+    {
+      targetUserId,
+      oldChannelId,
+      anonProfileResult
+    }: MoveToChatChannelParams & {anonProfileResult?: any},
+    channelCategory: ChannelCategory
+  ): Promise<void> => {
     if (!localDb) return;
+
     try {
-      const result = await moveChatToSigned({targetUserId, oldChannelId});
-      const messages = result?.data?.messageHistories?.map((item: any) => {
-        return item?.message;
-      });
+      const result = await (channelCategory === 'SIGNED'
+        ? moveChatToSigned({targetUserId, oldChannelId})
+        : moveChatToAnon({
+            targetUserId,
+            oldChannelId,
+            anon_user_info_color_code: anonProfileResult?.anon_user_info_color_code,
+            anon_user_info_color_name: anonProfileResult?.anon_user_info_color_name,
+            anon_user_info_emoji_code: anonProfileResult?.anon_user_info_emoji_code,
+            anon_user_info_emoji_name: anonProfileResult?.anon_user_info_emoji_name
+          }));
+
+      const messages = result?.data?.messageHistories?.map((item: any) => item?.message);
       const channel = {
         ...result?.data?.channel,
         members: result?.data?.better_channel_members,
         messages
       };
-      await saveChannelData(channel, 'SIGNED');
+
+      await saveChannelData(channel, channelCategory);
     } catch (err) {
-      console.log('error on moveToSignedChannel', err);
+      console.log(`error on moveTo${channelCategory}Channel`, err);
     }
   };
 
-  const moveToAnonymousChannel = async ({targetUserId, oldChannelId}: MoveToChatChannelParams) => {
-    if (!localDb) return;
-    try {
-      const anonProfileResult = await generateAnonProfileOtherProfile(targetUserId);
-      const result = await moveChatToAnon({
-        targetUserId,
-        oldChannelId,
-        anon_user_info_color_code: anonProfileResult.anon_user_info_color_code,
-        anon_user_info_color_name: anonProfileResult.anon_user_info_color_name,
-        anon_user_info_emoji_code: anonProfileResult.anon_user_info_emoji_code,
-        anon_user_info_emoji_name: anonProfileResult.anon_user_info_emoji_name
-      });
+  const moveToSignedChannel = async (params: MoveToChatChannelParams): Promise<void> => {
+    await moveToChannel(params, 'SIGNED');
+  };
 
-      const messages = result?.data?.messageHistories?.map((item: any) => {
-        return item?.message;
-      });
-      const channel = {
-        ...result?.data?.channel,
-        members: result?.data?.better_channel_members,
-        messages
-      };
-      await saveChannelData(channel, 'ANONYMOUS');
-    } catch (err) {
-      console.log('error on moveToAnonymousChannel', err);
-    }
+  const moveToAnonymousChannel = async (params: MoveToChatChannelParams): Promise<void> => {
+    const anonProfileResult = await generateAnonProfileOtherProfile(params.targetUserId);
+    await moveToChannel({...params, anonProfileResult}, 'ANONYMOUS');
   };
 
   return {
