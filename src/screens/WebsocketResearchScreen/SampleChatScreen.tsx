d67ce7eb8fdcb20e8b4f28457884c60e0a427de5
Ari Lukman P <ari.lukman.p@gmail.com>
2023-11-24 21:36:52 +0700

    feat: ping 2353 implement new chat screen design

--- src/screens/WebsocketResearchScreen/SampleChatScreen.tsx
+++ src/screens/WebsocketResearchScreen/SampleChatScreen.tsx
@@ -2,7 +2,17 @@
 /* eslint-disable import/no-unresolved */
 
 import * as React from 'react';
-import {Dimensions, FlatList, KeyboardAvoidingView, Platform, StyleSheet, View} from 'react-native';
+import Animated, {Easing, useAnimatedStyle, withTiming} from 'react-native-reanimated';
+import {
+  Dimensions,
+  FlatList,
+  KeyboardAvoidingView,
+  Platform,
+  StatusBar,
+  StyleSheet,
+  View
+} from 'react-native';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
 
 import AnonymousInputMessage from '../../components/Chat/AnonymousInputMessage';
 import BaseChatItem from '../../components/AnonymousChat/BaseChatItem';
@@ -44,6 +54,16 @@ export const styles = StyleSheet.create({
 });
 
 const SampleChatScreen = () => {
+  const inset = useSafeAreaInsets();
+  const animatedInset = useAnimatedStyle(() => {
+    return {
+      backgroundColor: colors.anon_primary,
+      paddingTop: withTiming(inset.top, {
+        duration: 300,
+        easing: Easing.inOut(Easing.quad)
+      })
+    };
+  });
   const flatlistRef = React.useRef<FlatList>();
   const {
     selectedChannel,
@@ -65,36 +85,40 @@ const SampleChatScreen = () => {
   };
 
   return (
-    <KeyboardAvoidingView
-      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-      style={styles.keyboardAvoidingView}
-      keyboardVerticalOffset={-500}>
-      {selectedChannel ? (
-        <ChatDetailHeader
-          onAvatarPress={goToChatInfoScreen}
-          onBackPress={goBackFromChatScreen}
-          onThreeDotPress={goToChatInfoScreen}
-          avatar={selectedChannel?.channelPicture}
-          user={selectedChannel?.name}
-          type={ANONYMOUS}
+    <Animated.View style={[{flex: 1}, animatedInset]}>
+      <StatusBar barStyle="light-content" backgroundColor={colors.anon_primary} />
+
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        style={styles.keyboardAvoidingView}
+        keyboardVerticalOffset={-500}>
+        {selectedChannel ? (
+          <ChatDetailHeader
+            onAvatarPress={goToChatInfoScreen}
+            onBackPress={goBackFromChatScreen}
+            onThreeDotPress={goToChatInfoScreen}
+            avatar={selectedChannel?.channelPicture}
+            user={selectedChannel?.name}
+            type={ANONYMOUS}
+          />
+        ) : null}
+        <FlatList
+          contentContainerStyle={{paddingBottom: 20}}
+          style={styles.chatContainer}
+          data={updatedChats}
+          inverted={true}
+          initialNumToRender={20}
+          alwaysBounceVertical={false}
+          bounces={false}
+          onLayout={scrollToEnd}
+          keyExtractor={(item, index) => item?.id || index.toString()}
+          renderItem={renderChatItem}
         />
-      ) : null}
-      <FlatList
-        contentContainerStyle={{paddingBottom: 20}}
-        style={styles.chatContainer}
-        data={updatedChats}
-        inverted={true}
-        initialNumToRender={20}
-        alwaysBounceVertical={false}
-        bounces={false}
-        onLayout={scrollToEnd}
-        keyExtractor={(item, index) => item?.id || index.toString()}
-        renderItem={renderChatItem}
-      />
-      <View style={styles.inputContainer}>
-        <AnonymousInputMessage onSendButtonClicked={sendChat} type={ANONYMOUS} />
-      </View>
-    </KeyboardAvoidingView>
+        <View style={styles.inputContainer}>
+          <AnonymousInputMessage onSendButtonClicked={sendChat} type={ANONYMOUS} />
+        </View>
+      </KeyboardAvoidingView>
+    </Animated.View>
   );
 };
 
