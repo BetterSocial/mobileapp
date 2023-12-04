02ce8b4c3d20552c62f8f1c11f4d567776221964
Ari Lukman P <ari.lukman.p@gmail.com>
2023-12-04 23:42:29 +0700

    feat: ping 2294 ping 3180 update useMessageHook for reply and delete message

--- src/hooks/screen/useMessageHook.ts
+++ src/hooks/screen/useMessageHook.ts
@@ -1,21 +1,71 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import {Alert, NativeSyntheticEvent} from 'react-native';
 import {ContextMenuOnPressNativeEvent} from 'react-native-context-menu-view';
-import {NativeSyntheticEvent} from 'react-native';
+import {useCallback, useContext} from 'react';
 
+import ChatSchema from '../../database/schema/ChatSchema';
 import ShareUtils from '../../utils/share';
 import UseMessageHook from '../../../types/hooks/screens/useMessageHook.types';
+import useLocalDatabaseHook from '../../database/hooks/useLocalDatabaseHook';
+import {Context} from '../../context';
+import {setReplyTarget} from '../../context/actions/chat';
 
 function useMessageHook(): UseMessageHook {
+  const {localDb, refresh} = useLocalDatabaseHook();
+  const [replyPreview, dispatch] = (useContext(Context) as unknown as any).chat;
+
+  const setReplyPreview = useCallback((messageItem) => {
+    console.log('Reply Message');
+    setReplyTarget(messageItem, dispatch);
+  }, []);
+
+  const clearReplyPreview = useCallback(() => {
+    console.log('clear Reply target');
+    setReplyTarget(null, dispatch);
+  }, []);
+
+  const deleteMessage = (messageId: string) => {
+    if (!localDb) return;
+    try {
+      ChatSchema.updateDeletedChatType(localDb, messageId);
+      refresh('chat');
+    } catch (error) {
+      console.log('error on delete message:', error);
+    }
+  };
+
   const onContextMenuPressed = (
     e: NativeSyntheticEvent<ContextMenuOnPressNativeEvent>,
+    messageId: string,
     message: string
   ) => {
     const event = e.nativeEvent;
+    if (event.index === 0) {
+      const messageItem = {
+        username: 'Anonymous User',
+        time: '4h',
+        message: 'sdfgsdMollit esse ea anim exercitation irure quis nostrud est.',
+        messageId: 'sdfs',
+        chatType: '',
+        messageType: 'regular'
+      };
+      setReplyPreview(messageItem);
+    }
     if (event.index === 1) {
       ShareUtils.copyTextToClipboard(message);
+    }
+    if (event.index === 2) {
+      Alert.alert('Delete Message?', '', [
+        {text: 'Cancel', style: 'cancel'},
+        {text: 'Confirm', style: 'destructive', onPress: () => deleteMessage(messageId)}
+      ]);
     }
   };
 
   return {
+    replyPreview: replyPreview?.replyTarget,
+    setReplyPreview,
+    clearReplyPreview,
     onContextMenuPressed
   };
 }
--- types/hooks/screens/useMessageHook.types.ts
+++ types/hooks/screens/useMessageHook.types.ts
@@ -2,8 +2,12 @@ import {ContextMenuOnPressNativeEvent} from 'react-native-context-menu-view';
 import {NativeSyntheticEvent} from 'react-native';
 
 interface UseMessageHook {
+  replyPreview: any;
+  setReplyPreview: (any) => void;
+  clearReplyPreview: () => void;
   onContextMenuPressed: (
     e: NativeSyntheticEvent<ContextMenuOnPressNativeEvent>,
+    messageId: string,
     message: string
   ) => void;
 }
