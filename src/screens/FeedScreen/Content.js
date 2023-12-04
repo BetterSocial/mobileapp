082f6b1784677e4f7fc147f4194650c1ec716ea6
agitafirstawan26@gmail.com <agitafirstawan26@gmail.com>
2023-12-04 19:00:16 +0700

    fix: rollback content

--- src/screens/FeedScreen/Content.js
+++ src/screens/FeedScreen/Content.js
@@ -72,6 +72,9 @@ const Content = ({
 
       return result >= 0 ? result : 0;
     }
+    if (getCommentLength(item.latest_reactions.comment) > 0) {
+      return Math.floor(layoutHeight / lineHeight);
+    }
     return Math.round(layoutHeight / lineHeight);
   };
 
