4437bff41445515cfa282f99c811bbfe0f9df7c8
Yusuf Putra <yusufputra99@gmail.com>
2024-01-04 21:07:37 +0700

    feat: mas agita code

--- src/screens/ProfileScreen/index.js
+++ src/screens/ProfileScreen/index.js
@@ -501,6 +501,7 @@ const ProfileScreen = ({route}) => {
   };
 
   const debounceModalOpen = debounce(() => {
+    setTempBio(profile?.myProfile?.bio);
     bottomSheetBioRef.current.open();
   }, 350);
 
@@ -617,7 +618,6 @@ const ProfileScreen = ({route}) => {
     if (isProfileTabSigned) return getMyFeeds();
     return reloadFetchAnonymousPost();
   };
-
   return (
     <View style={styles.container} forceInset={{top: 'always'}}>
       <StatusBar translucent={false} />
