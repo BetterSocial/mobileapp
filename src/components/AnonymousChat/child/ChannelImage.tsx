2f3e8a0b4f17cee393ee6cd039c9f4eca6277874
asaadam <graha.asa@gmail.com>
2024-01-30 22:54:49 +0700

    chore: fix import file

--- src/components/AnonymousChat/child/ChannelImage.tsx
+++ src/components/AnonymousChat/child/ChannelImage.tsx
@@ -1,17 +1,17 @@
 import * as React from 'react';
+import {StyleSheet, View} from 'react-native';
 import FastImage from 'react-native-fast-image';
-import {StyleSheet, Text, View} from 'react-native';
 
+import {BaseChannelItemTypeProps} from '../../../../types/component/AnonymousChat/BaseChannelItem.types';
+import {AnonUserInfo} from '../../../../types/service/AnonProfile.type';
+import ChatIcon from '../../../assets/chat-icon.png';
 import AnonymousProfile from '../../../assets/images/AnonymousProfile.png';
+import FeedIcon from '../../../assets/images/feed-icon.png';
+import useProfileHook from '../../../hooks/core/profile/useProfileHook';
+import dimen from '../../../utils/dimen';
+import {COLORS} from '../../../utils/theme';
 import ChannelAnonymousImage from './ChannelAnonymousImage';
 import ChannelAnonymousSubImage from './ChannelAnonymousSubImage';
-import ChatIcon from '../../../assets/chat-icon.png';
-import FeedIcon from '../../../assets/images/feed-icon.png';
-import dimen from '../../../utils/dimen';
-import useProfileHook from '../../../hooks/core/profile/useProfileHook';
-import {AnonUserInfo} from '../../../../types/service/AnonProfile.type';
-import {BaseChannelItemTypeProps} from '../../../../types/component/AnonymousChat/BaseChannelItem.types';
-import {COLORS} from '../../../utils/theme';
 
 export type ChannelImageProps = {
   mainPicture?: string;
