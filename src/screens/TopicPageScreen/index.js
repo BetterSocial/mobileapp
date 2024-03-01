62c1e83e934196df3b92035fb7dbf9b686e4bdba
Alif Alamsyah <alamsyaah1198@gmail.com>
2024-03-01 23:05:18 +0700

    fix: default object

--- src/screens/TopicPageScreen/index.js
+++ src/screens/TopicPageScreen/index.js
@@ -162,7 +162,7 @@ const TopicPageScreen = (props) => {
     try {
       const resultTopicDetail = await getTopics(domain);
       if (resultTopicDetail.data) {
-        const detail = resultTopicDetail.data.find((item) => item.name === domain) || {};
+        const detail = resultTopicDetail.data.find((item) => item.name === domain);
         setTopicDetail(detail);
         handleFollowData(detail.is_followed_by);
         setMemberCount(Number(detail.followersCount));
@@ -469,7 +469,7 @@ const TopicPageScreen = (props) => {
       <BottomSheetFollow
         ref={bottomSheetFollowRef}
         onClose={() => bottomSheetFollowRef.current.close()}
-        topicName={topicDetail.name}
+        topicName={topicDetail?.name}
         memberCount={memberCount}
         setMemberCount={setMemberCount}
         isFollow={isFollow}
