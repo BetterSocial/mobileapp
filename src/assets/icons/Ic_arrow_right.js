027d0e713251f11007e563fe611f8fd84bbb3631
GarryLaly <garrylaly@gmail.com>
2024-03-26 22:48:02 +0700

    feat: ping 3842 empty anon chat in progress

--- src/assets/icons/Ic_arrow_right.js
+++ src/assets/icons/Ic_arrow_right.js
@@ -8,7 +8,7 @@ function Ic_arrow_right(props) {
         fillRule="evenodd"
         clipRule="evenodd"
         d="M.293.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L5.586 7 .293 1.707a1 1 0 010-1.414z"
-        fill="#000"
+        fill={props.fill || '#000'}
       />
     </Svg>
   );
