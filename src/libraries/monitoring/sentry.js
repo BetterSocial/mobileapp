7ab3116469ec014db1747e8ef93c36b3727d5947
Muhammad Fajariansyah Ismail <fajarism@RokaM1.local>
2024-09-24 21:11:21 +0700

    build: adjust project to support macos 16

--- src/libraries/monitoring/sentry.js
+++ src/libraries/monitoring/sentry.js
@@ -1,42 +1,54 @@
-import * as Sentry from '@sentry/react-native';
+// import * as Sentry from '@sentry/react-native';
+
+// const SentryMonitoring = () => {
+//   return {
+//     setUser({id}) {
+//       Sentry.setUser({
+//         id
+//       });
+//     },
+//     logActions(message, data) {
+//       Sentry.addBreadcrumb({
+//         type: 'Info',
+//         level: 'info',
+//         message,
+//         data: data ?? {}
+//       });
+//     },
+//     logError(message, data) {
+//       Sentry.addBreadcrumb({
+//         type: 'Error',
+//         level: 'error',
+//         message,
+//         data
+//       });
+//     },
+//     logNavigation(from, to) {
+//       if (from && to) {
+//         Sentry.addBreadcrumb({
+//           type: 'navigation',
+//           data: {
+//             from,
+//             to
+//           }
+//         });
+//       }
+//     },
+//     clearUser() {
+//       Sentry.configureScope((scope) => scope.setUser(null));
+//     }
+//   };
+// };
+
+// export const Monitoring = SentryMonitoring();
 
 const SentryMonitoring = () => {
   return {
-    setUser({id}) {
-      Sentry.setUser({
-        id
-      });
-    },
-    logActions(message, data) {
-      Sentry.addBreadcrumb({
-        type: 'Info',
-        level: 'info',
-        message,
-        data: data ?? {}
-      });
-    },
-    logError(message, data) {
-      Sentry.addBreadcrumb({
-        type: 'Error',
-        level: 'error',
-        message,
-        data
-      });
-    },
-    logNavigation(from, to) {
-      if (from && to) {
-        Sentry.addBreadcrumb({
-          type: 'navigation',
-          data: {
-            from,
-            to
-          }
-        });
-      }
-    },
-    clearUser() {
-      Sentry.configureScope((scope) => scope.setUser(null));
-    }
+    setUser({id}) {},
+    logActions(message, data) {},
+    logError(message, data) {},
+    logNavigation(from, to) {},
+    clearUser() {}
   };
 };
 
